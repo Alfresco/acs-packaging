8c0bb48d07f57c37eb635da85b53d317b4639ca1
Constantin Popa <constantin.popa@ness.com>
2019-05-30 08:39:06 +0100

    APPSREPO-656 : ACS Deployment includes Sync service    - check that the 'org_alfresco_device_sync_repo' amp is installed    - included the sync service api tests in the restapi-acs-all-amps-suite.xml suite

--- tests/tas-restapi/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
+++ tests/tas-restapi/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
@@ -100,9 +100,11 @@ public class DiscoveryTests extends RestTest
         assertTrue(modules.contains("alfresco-aos-module"));
         assertTrue(modules.contains("org.alfresco.integrations.google.docs"));
         assertTrue(modules.contains("alfresco-share-services"));
+        assertTrue(modules.contains("org_alfresco_device_sync_repo"));
+
 
         // Check that all installed modules are in INSTALLED state
         List<String> modulesStates = restClient.onResponse().getResponse().jsonPath().getList("entry.repository.modules.installState", String.class);
-        assertEquals(Collections.frequency(modulesStates, "INSTALLED"), 3);
+        assertEquals(Collections.frequency(modulesStates, "INSTALLED"), 4);
     }
 }
