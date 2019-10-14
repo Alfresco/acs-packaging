f2e40fec96161f150560bf8fe00d721e6c1707da
Alex Mukha <killerboot@users.noreply.github.com>
2019-10-14 12:12:39 +0100

    Take out tests from startup script

--- tests/scripts/start-compose.sh
+++ tests/scripts/start-compose.sh
@@ -19,9 +19,3 @@ else
   echo "Docker Compose failed to start" >&2
   exit 1
 fi
-
-# find this script's directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-source ${SCRIP_DIR}/wait-for-alfresco-start.sh "http://localhost:8081/alfresco"
-source ${SCRIP_DIR}/wait-for-alfresco-start.sh "http://localhost:8082/alfresco"
