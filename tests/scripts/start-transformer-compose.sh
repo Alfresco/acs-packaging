a812de643d3e261f4c9a1bd32cb89cbc706d8142
David Edwards <david.edwards@alfresco.com>
2019-10-30 14:02:42 +0000

    REPO-4727
    Updated travis.yml
    Added script to launch docker compose for transformers, providing ability to select with of the 3 services (LOCAL, LEGACY, TRANSFORM) are enabled. Added single docker-compose file for transformers.
    Removed unused compose files

---
+++ tests/scripts/start-transformer-compose.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+START_COMPOSE_PATH=$1
+DOCKER_COMPOSE_PATH=$2
+
+if [[ -z "$DOCKER_COMPOSE_PATH" ]] || [[ -z "$START_COMPOSE_PATH" ]];
+then
+  echo "To launch compose with all transform services enabled please provide paths to start_compose.sh and docker-compose.yml:"
+  echo "\"${0##*/} /path/to/start_compose.sh /path/to/docker-compose.yml\" ."
+  echo "To enable one or more transform service this can be followed with the following flags: \"--legacy\", \"--local\", and \"--transformservice\""
+  exit 1
+fi
+
+docker info
+docker-compose --version
+
+# NOTE
+# In ACS 6.2 existing LOCAL transformers were renamed to LEGACY
+# For < ACS 6.2 please set LOCAL_TRANSFORM_SERVICE_ENABLED=true
+
+CUSTOM_DETECTED=false 
+for arg in "$@"
+do
+    if [ "$arg" == "--legacy" ] || [ "$arg" == "--local" ] || [ "$arg" == "--transformservice" ]
+    then
+      if [ "$CUSTOM_DETECTED" == false ]
+      then
+        export LEGACY_TRANSFORM_SERVICE_ENABLED=false
+        export LOCAL_TRANSFORM_SERVICE_ENABLED=false
+        export TRANSFORM_SERVICE_ENABLED=false
+        CUSTOM_DETECTED=true
+      fi
+      case $arg in
+        --legacy )
+          export LEGACY_TRANSFORM_SERVICE_ENABLED=TRUE
+          echo "LEGACY_TRANSFORM_SERVICE_ENABLED=${LEGACY_TRANSFORM_SERVICE_ENABLED}" ;;
+        --local )
+          export LOCAL_TRANSFORM_SERVICE_ENABLED=TRUE
+          echo "LOCAL_TRANSFORM_SERVICE_ENABLED=${LOCAL_TRANSFORM_SERVICE_ENABLED}" ;;
+        --transformservice )
+          export TRANSFORM_SERVICE_ENABLED=TRUE 
+          echo "TRANSFORM_SERVICE_ENABLED=${TRANSFORM_SERVICE_ENABLED}";;
+      esac
+    fi
+done
+if [ $LEGACY_TRANSFORM_SERVICE_ENABLED == false ]
+then
+  echo "LEGACY_TRANSFORM_SERVICE_ENABLED=${LEGACY_TRANSFORM_SERVICE_ENABLED}"
+fi
+if [ $LOCAL_TRANSFORM_SERVICE_ENABLED == false ]
+then
+  echo "LOCAL_TRANSFORM_SERVICE_ENABLED=${LOCAL_TRANSFORM_SERVICE_ENABLED}"
+fi
+if [ $TRANSFORM_SERVICE_ENABLED == false ]
+then
+  echo "TRANSFORM_SERVICE_ENABLED=${TRANSFORM_SERVICE_ENABLED}"
+fi
+
+$START_COMPOSE_PATH $DOCKER_COMPOSE_PATH
+
