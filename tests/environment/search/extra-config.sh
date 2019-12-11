62158b6533103d5d6bf01b22cd28690305af648a
Alex Mukha <alex.mukha@alfresco.com>
2019-12-11 10:12:02 +0000

    Correct the crons for feeds and search

--- tests/environment/search/extra-config.sh
+++ tests/environment/search/extra-config.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Runs trackers once a second by modifying the template config of cores
-sed -i 's|alfresco.cron=0/10 \* \* \* \* ? \*|alfresco.cron=0/1 \* \* \* \* ?|' \
+sed -i 's|alfresco.cron=0/10 \* \* \* \* ? \*|alfresco.cron=\* \* \* ? \* \*|' \
     /opt/alfresco-search-services/solrhome/templates/rerank/conf/solrcore.properties
 sed -i 's/alfresco.commitInterval=2000/alfresco.commitInterval=200/' \
     /opt/alfresco-search-services/solrhome/templates/rerank/conf/solrcore.properties
