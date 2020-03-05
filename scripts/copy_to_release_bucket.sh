298829178af74994af985c2632fa1c6a5dc90ac4
Erik Knizat <erik.knizat@alfresco.com>
2020-03-05 20:11:33 +0000

    [publish] try the copy to release s3 again

--- scripts/copy_to_release_bucket.sh
+++ scripts/copy_to_release_bucket.sh
@@ -4,14 +4,27 @@ set -ev
 #
 # Copy from S3 Release bucket to S3 eu.dl bucket
 #
+echo "------ Running the copy to release bucket script -------"
+echo "Travis branch:  $TRAVIS_BRANCH"
+echo "Travis build number: $TRAVIS_BUILD_NUMBER"
+echo "Release version: $RELEASE_VERSION"
 
-build_number=$1
-branch_name=$2
-release_version=$3
+if [ -z ${RELEASE_VERSION} ];
+then
+  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
+  exit -1
+fi
+
+
 bamboo_stage=release
-SOURCE=s3://alfresco-artefacts-staging/alfresco-content-services/snapshots/test/ek/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}
+
+SOURCE=s3://alfresco-artefacts-staging/alfresco-content-services/snapshots/test/ek/feature/repo-4736-release_stage/1731
 DESTINATION=s3://eu.dl.alfresco.com/release/enterprise/ek/ACS/${RELEASE_VERSION:0:3}/$RELEASE_VERSION/$TRAVIS_BUILD_NUMBER
-#SOURCE=s3://alfresco-artefacts-staging/alfresco-content-services/release/$branch_name/$build_number
+
+#SOURCE=s3://alfresco-artefacts-staging/alfresco-content-services/snapshots/test/ek/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}
+#DESTINATION=s3://eu.dl.alfresco.com/release/enterprise/ek/ACS/${RELEASE_VERSION:0:3}/$RELEASE_VERSION/$TRAVIS_BUILD_NUMBER
+
+#SOURCE=s3://alfresco-artefacts-staging/alfresco-content-services/release/$TRAVIS_BRANCH/$TRAVIS_BUILD_NUMBER
 #DESTINATION=s3://eu.dl.alfresco.com/release/enterprise/ACS/${RELEASE_VERSION:0:3}/$RELEASE_VERSION/$TRAVIS_BUILD_NUMBER
 
 
@@ -20,4 +33,4 @@ $SOURCE\n
 $DESTINATION\n"
 
 aws s3 cp --acl private $SOURCE/alfresco.war $DESTINATION/alfresco.war
-aws s3 cp --acl private $SOURCE/alfresco-content-services-distribution-$release_version.zip $DESTINATION/alfresco-content-services-distribution-$release_version.zip
+aws s3 cp --acl private $SOURCE/alfresco-content-services-distribution-$RELEASE_VERSION.zip $DESTINATION/alfresco-content-services-distribution-$RELEASE_VERSION.zip
