8e89980b2a4c9bfed40d8023637037dbba261250
Erik Knizat <erik.knizat@alfresco.com>
2020-03-05 18:33:06 +0000

    Set quiete mode

--- scripts/maven_release.sh
+++ scripts/maven_release.sh
@@ -12,7 +12,7 @@ if [ -z ${RELEASE_VERSION} ] || [ -z ${DEVELOPMENT_VERSION} ];
     then echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
          exit -1
 else   
-    mvn --batch-mode \
+    mvn --batch-mode -q \
     -Dusername="${GIT_USERNAME}" \
     -Dpassword="${GIT_PASSWORD}" \
     -DreleaseVersion=${RELEASE_VERSION} \
