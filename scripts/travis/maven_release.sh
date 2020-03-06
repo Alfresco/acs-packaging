4a2d5179594281853fc8998a9ce4db72068c757e
Erik Knizat <erik.knizat@alfresco.com>
2020-03-06 17:35:06 +0000

    [release] [publish] uncomment out all of travis yaml functionality again..

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-#export scm_path=$(mvn help:evaluate -Dexpression=project.scm.url -q -DforceStdout)
-
 # Use full history for release
 git checkout -B "${TRAVIS_BRANCH}"
 # Add email to link commits to user
@@ -26,22 +24,19 @@ else
 #    release:clean release:prepare release:perform \
 #    -Prelease
 
-    mvn -X --batch-mode -DscmCommentPrefix="[maven-release-plugin][skip ci] " -Dusername="${GIT_USERNAME}" -Dpassword="${GIT_PASSWORD}" -DreleaseVersion=${RELEASE_VERSION} -DdevelopmentVersion=${DEVELOPMENT_VERSION} -DskipTests -Darguments="-DskipTests" -Prelease release:clean release:prepare release:perform
+#    mvn -X --batch-mode -DscmCommentPrefix="[maven-release-plugin][skip ci] " -Dusername="${GIT_USERNAME}" -Dpassword="${GIT_PASSWORD}" -DreleaseVersion=${RELEASE_VERSION} -DdevelopmentVersion=${DEVELOPMENT_VERSION} -DskipTests -Darguments="-DskipTests" -Prelease release:clean release:prepare release:perform
 
 
-
-#     mvn --batch-mode -q \
-#    -Dusername="${GIT_USERNAME}" \
-#    -Dpassword="${GIT_PASSWORD}" \
-#    -DreleaseVersion=${RELEASE_VERSION} \
-#    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
-#    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
-#    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
-#    -Dversion.edition="Enterprise" \
-#    -Dscm-path=${scm_path} \
-#    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
-#    -DskipTests \
-#    "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
-#    release:clean release:prepare release:perform \
-#    -Prelease
+    mvn --batch-mode -q \
+    -DskipTests \
+    -Dusername="${GIT_USERNAME}" \
+    -Dpassword="${GIT_PASSWORD}" \
+    -DreleaseVersion=${RELEASE_VERSION} \
+    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
+    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
+    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
+    -Dversion.edition="Enterprise" \
+    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
+    "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}'" \
+    -Prelease release:clean release:prepare release:perform
 fi
