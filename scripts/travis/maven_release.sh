807227b4ae45e8fb0373d5adae0275b23ed9fe56
Erik Knizat <erik.knizat@alfresco.com>
2020-03-06 10:58:25 +0000

    [release] try to "pre" maven install

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -11,19 +11,19 @@ git config user.email "${GIT_EMAIL}"
 if [ -z ${RELEASE_VERSION} ] || [ -z ${DEVELOPMENT_VERSION} ];
     then echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
          exit -1
-#else
-#    mvn --batch-mode -q \
-#    -Dusername="${GIT_USERNAME}" \
-#    -Dpassword="${GIT_PASSWORD}" \
-#    -DreleaseVersion=${RELEASE_VERSION} \
-#    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
-#    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
-#    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
-#    -Dversion.edition="Enterprise" \
-#    -Dscm-path=${scm_path} \
-#    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
-#    -DskipTests \
-#    "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
-#    release:clean release:prepare release:perform \
-#    -Prelease
+else
+    mvn --batch-mode -q \
+    -Dusername="${GIT_USERNAME}" \
+    -Dpassword="${GIT_PASSWORD}" \
+    -DreleaseVersion=${RELEASE_VERSION} \
+    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
+    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
+    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
+    -Dversion.edition="Enterprise" \
+    -Dscm-path=${scm_path} \
+    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
+    -DskipTests \
+    "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
+    release:clean release:prepare release:perform \
+    -Prelease
 fi
