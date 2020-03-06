931f6dc3e584cc332d80772d92efc684550d74f2
Erik Knizat <erik.knizat@alfresco.com>
2020-03-06 13:49:35 +0000

    [release] Try just the maven  release

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export scm_path=$(mvn help:evaluate -Dexpression=project.scm.url -q -DforceStdout)
+#export scm_path=$(mvn help:evaluate -Dexpression=project.scm.url -q -DforceStdout)
 
 # Use full history for release
 git checkout -B "${TRAVIS_BRANCH}"
@@ -15,15 +15,30 @@ else
     mvn --batch-mode -q \
     -Dusername="${GIT_USERNAME}" \
     -Dpassword="${GIT_PASSWORD}" \
-    -DreleaseVersion=${RELEASE_VERSION} \
-    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
+    -DreleaseVersion=6.3.0-test-release-repo-4736-4 \
+    -DdevelopmentVersion=6.3.0-test-1-SNAPSHOT \
     -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
     -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
     -Dversion.edition="Enterprise" \
-    -Dscm-path=${scm_path} \
     -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
     -DskipTests \
     "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
     release:clean release:prepare release:perform \
     -Prelease
+
+
+#     mvn --batch-mode -q \
+#    -Dusername="${GIT_USERNAME}" \
+#    -Dpassword="${GIT_PASSWORD}" \
+#    -DreleaseVersion=${RELEASE_VERSION} \
+#    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
+#    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
+#    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
+#    -Dversion.edition="Enterprise" \
+#    -Dscm-path=${scm_path} \
+#    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
+#    -DskipTests \
+#    "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -Dscm-path=${scm_path} " \
+#    release:clean release:prepare release:perform \
+#    -Prelease
 fi
