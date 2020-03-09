768fc0c16a9324cc898b3cfc7acf47fe27fe55cf
Erik Knizat <erik.knizat@alfresco.com>
2020-03-09 14:14:57 +0000

    [release] [publish] Try running mvn install during the install stage to prepopulate m2 cache

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -10,23 +10,7 @@ if [ -z ${RELEASE_VERSION} ] || [ -z ${DEVELOPMENT_VERSION} ];
     then echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
          exit -1
 else
-#    mvn --batch-mode -X \
-#    -Dusername="${GIT_USERNAME}" \
-#    -Dpassword="${GIT_PASSWORD}" \
-#    -DreleaseVersion=${RELEASE_VERSION} \
-#    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
-#    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
-#    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
-#    -Dversion.edition="Enterprise" \
-#    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
-#    -DskipTests \
-#    "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}'" \
-#    release:clean release:prepare release:perform \
-#    -Prelease
-
-#    mvn -X --batch-mode -DscmCommentPrefix="[maven-release-plugin][skip ci] " -Dusername="${GIT_USERNAME}" -Dpassword="${GIT_PASSWORD}" -DreleaseVersion=${RELEASE_VERSION} -DdevelopmentVersion=${DEVELOPMENT_VERSION} -DskipTests -Darguments="-DskipTests" -Prelease release:clean release:prepare release:perform
-
-    mvn --batch-mode -X \
+    mvn --batch-mode \
     -DskipTests \
     -Dusername="${GIT_USERNAME}" \
     -Dpassword="${GIT_PASSWORD}" \
