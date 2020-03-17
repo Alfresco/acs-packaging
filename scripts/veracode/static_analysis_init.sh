af5e5a1ed2e02c574e81267f85f46654a8286db4
Nithin Nambiar <nithinnambiar@yahoo.co.in>
2020-03-17 15:54:55 +0000

    REPO-4906 refactor veracode static analysis refactoring

---
+++ scripts/veracode/static_analysis_init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Static Analysis Init Script ==========================="
+
+wget https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$VERACODE_WRAPPER_VERSION/vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar
+
+echo "=========================== Finishing Static Analysis Init Script =========================="
