cabe185f7fd19a928315143a81a661f5fec9c9af
NITHIN NAMBIAR <nithin.nambiar@alfresco.com>
2020-03-30 16:25:31 +0100

    veracode static scan - add sandbox config

--- scripts/veracode/static_analysis.sh
+++ scripts/veracode/static_analysis.sh
@@ -3,7 +3,7 @@ set -e
 echo "=========================== Starting Static Analysis Script ==========================="
 
 java -jar vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar -vid $VERACODE_API_ID -vkey $VERACODE_API_KEY \
-          -action uploadandscan -appname "ACS Repository" -sandboxname "ACS Repo Team Sandbox" -createprofile false \
+          -action uploadandscan -appname "ACS Repository" -sandboxname ${VERACODE_SANDBOX} -createprofile false \
            -filepath war/target/alfresco.war -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER"
 
 
