40bf2bcedc0a5c3fe7b8f857914c3b52d36adbfe
Alan Davis <alan.davis@alfresco.com>
2020-04-04 14:50:12 +0100

    REPO-4710 comment out media management (mm) and document-transformation-engine (dte) from the all-amps test as they are based on legacy transforms.

--- tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
+++ tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
@@ -77,9 +77,11 @@ public class DiscoveryTests extends RestTest
                 "alfresco-share-services",
                 "alfresco-saml-repo",
                 "org_alfresco_device_sync_repo",
-                "org_alfresco_mm_repo", "alfresco-ai-repo",
+//                "org_alfresco_mm_repo",
+                "alfresco-ai-repo",
                 "alfresco-glacier-connector-repo",
-                "org.alfresco.module.TransformationServer");
+//                "org.alfresco.module.TransformationServer" // this is the DTE not ATS
+        );
 
         expectedModules.forEach(module ->
                 assertTrue(modules.contains(module), String.format("Expected module %s is not installed", module)));
