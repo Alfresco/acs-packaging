b236774bc0cb2fec32fe4378d2f470f123f7ede6
Ancuta Morarasu <ancuta.morarasu@ness.com>
2020-04-25 18:27:30 +0300

    MNT-21417: Fix the waiting time in the test

--- tests/tas-integration/src/test/java/org/alfresco/tas/integration/subsystems/ldap/LdapUserSynchronizationTest.java
+++ tests/tas-integration/src/test/java/org/alfresco/tas/integration/subsystems/ldap/LdapUserSynchronizationTest.java
@@ -30,8 +30,8 @@ public class LdapUserSynchronizationTest extends IntegrationTest
         jmxBuilder.getJmxClient().writeProperty("Alfresco:Category=Synchronization,Type=Configuration,id1=default", "synchronization.syncOnStartup", "false");
     }
 
-    @Test(groups = { TestGroup.LDAP })
-    @TestRail(section = { TestGroup.LDAP }, executionType = ExecutionType.SANITY, description = "Verify if the user synchronization job runs after a property was changed (the subsystem is restarted)")
+    @Test(groups = {TestGroup.LDAP})
+    @TestRail(section = {TestGroup.LDAP}, executionType = ExecutionType.SANITY, description = "Verify if the user synchronization job runs after a property was changed (the subsystem is restarted)")
     public void triggerSyncJobAndCheckStatus() throws Exception
     {
         STEP("1. Change the cron expression -> causes the subsystem to restart");
@@ -48,11 +48,14 @@ public class LdapUserSynchronizationTest extends IntegrationTest
 
         STEP("2. Trigger the ldap users sync job");
         jmxBuilder.getJmxClient().executeJMXMethod("Alfresco:Name=Schedule,Group=DEFAULT,Type=MonitoredCronTrigger,Trigger=syncTrigger", "executeNow");
-        Utility.waitToLoopTime(2, "Waiting for sync job to finish.");
-        String syncStatusNewExecutionTime = jmxBuilder.getJmxClient().readProperty("Alfresco:Name=BatchJobs,Type=Synchronization,Category=manager", "SyncEndTime").toString();
 
-        STEP("3. Verify the execution time of the sync job");
-        Assert.assertNotEquals(syncStatusPrevExecutionTime, syncStatusNewExecutionTime,
-                String.format("The ldap user sync job didn't execute.\nPrevious run time: [%s]\nNew run time: [%s]", syncStatusPrevExecutionTime, syncStatusNewExecutionTime));
+        // Wait for the sync job to finish
+        Utility.sleep(300, 5000, () ->
+        {
+            STEP("3. Verify the execution time of the sync job");
+            String syncStatusNewExecutionTime = jmxBuilder.getJmxClient().readProperty("Alfresco:Name=BatchJobs,Type=Synchronization,Category=manager", "SyncEndTime").toString();
+            Assert.assertNotEquals(syncStatusPrevExecutionTime, syncStatusNewExecutionTime,
+                    String.format("The ldap user sync job didn't execute.\nPrevious run time: [%s]\nNew run time: [%s]", syncStatusPrevExecutionTime, syncStatusNewExecutionTime));
+        });
     }
 }
