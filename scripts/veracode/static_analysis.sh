9b64bc80db2b61e8ae826ad0a549aa842d7b6ae5
DenisGabriela <Denis.Ungureanu1@ness.com>
2020-05-04 17:49:37 +0300

    ACS-102 : Veracode build instability in acs-packaging    - add retry on failed veracode scan attempts    - add a wait between retries    - increase number of travis retries to 6

--- scripts/veracode/static_analysis.sh
+++ scripts/veracode/static_analysis.sh
@@ -3,8 +3,9 @@ set -e
 echo "=========================== Starting Static Analysis Script ==========================="
 
 java -jar vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar -vid $VERACODE_API_ID -vkey $VERACODE_API_KEY \
-          -action uploadandscan -appname "ACS Repository" -sandboxname "${VERACODE_SANDBOX}" -createprofile false \
-           -filepath war/target/alfresco.war -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER"
+          -action uploadandscan -appname "ACS Repository" -sandboxname "Denis Ungureanu s Sandbox" -createprofile false \
+           -filepath war/target/alfresco.war -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER" \
+|| (sleep 3m; exit 1) # wait 3 minutes before retry
 
 
 echo "=========================== Finishing Static Analysis Script =========================="
