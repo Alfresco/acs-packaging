f29eb7e47447f71992b747bf677f6c6cacd1ed66
DenisGabriela <Denis.Ungureanu1@ness.com>
2020-05-04 18:02:27 +0300

    ACS-102 : Veracode build instability in acs-packaging

--- scripts/veracode/static_analysis.sh
+++ scripts/veracode/static_analysis.sh
@@ -5,7 +5,7 @@ echo "=========================== Starting Static Analysis Script ==============
 java -jar vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar -vid $VERACODE_API_ID -vkey $VERACODE_API_KEY \
           -action uploadandscan -appname "ACS Repository" -sandboxname "Denis Ungureanu s Sandbox" -createprofile false \
            -filepath war/target/alfresco.war -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER" \
-|| (sleep 3m; exit 1) # wait 3 minutes before retry
+|| (sleep 5m; exit 1) # wait 5 minutes before retry
 
 
 echo "=========================== Finishing Static Analysis Script =========================="
