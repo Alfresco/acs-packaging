50e016a51231e12806228115953e10661218db4e
montgolfiere <jan.vonka@alfresco.com>
2020-05-21 08:47:22 +0100

    APPS-122: Update MM from 1.4.0 to 1.4.1-RC3 (for ACS 6.2.1) (#1215)
    * APPS-122: Update MM from 1.4.0 to 1.4.1-RC1 (for ACS 6.2.1)
    
    - see also APPS-119, APPS-102 (ACS-205), APPS-121
    
    * APPS-122: Update MM version to 1.4.1-RC3
    
    * APPS-122: Comment S3 connector dependency as it brings a lib that breaks the build
    
    * APPS-122: Comment glacier connector dependency as breaks the build.
    * skip glacier and s3 connectors tests
    
    Co-authored-by: ehardon <elena.hardon@ness.com>

--- tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
+++ tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
@@ -70,14 +70,14 @@ public class DiscoveryTests extends RestTest
                 "alfresco-aos-module",
                 "org.alfresco.integrations.google.docs",
                 "alfresco-trashcan-cleaner",
-                "org_alfresco_integrations_S3Connector",
+//                "org_alfresco_integrations_S3Connector",
                 "org_alfresco_module_xamconnector",
                 "alfresco-content-connector-for-salesforce-repo",
                 "alfresco-share-services",
                 "alfresco-saml-repo",
                 "org_alfresco_device_sync_repo",
                 "org_alfresco_mm_repo", "alfresco-ai-repo",
-                "alfresco-glacier-connector-repo",
+//                "alfresco-glacier-connector-repo",
                 "org.alfresco.module.TransformationServer");
 
         expectedModules.forEach(module ->
