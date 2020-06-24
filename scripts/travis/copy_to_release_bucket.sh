27d9b499e91797edafcab28e4e0f6514fbef978c
DenisGabriela <Denis.Ungureanu1@ness.com>
2020-06-24 10:59:27 +0300

    REPO-5072 : Update version of acs-packaging to 7.0.0

--- scripts/travis/copy_to_release_bucket.sh
+++ scripts/travis/copy_to_release_bucket.sh
@@ -7,7 +7,7 @@ set -ev
 
 if [ -z ${RELEASE_VERSION} ];
 then
-  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (7.0-EA or 7.0-SNAPSHOT)"
+  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
   exit -1
 fi
 
