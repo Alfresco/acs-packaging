4f684df37101a4b76da6006149913730cedce1d3
CezarLeahu <35226487+cezarleahu@users.noreply.github.com>
2020-06-29 08:37:24 +0300

    ACS-381: MM 1.4.2-RC1 release
    * ACS-381: MM 1.4.2-RC1 release
    
    - update MM version from 1.4.1 to 1.4.2-RC1
    - enable MM AMPs in the tas-all-amps module
    
    * Uncommented mm
    
    Co-authored-by: Cristian Turlica <cristian.turlica@ness.com>

--- tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
+++ tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
@@ -78,9 +78,10 @@ public class DiscoveryTests extends RestTest
                 "alfresco-share-services",
                 "alfresco-saml-repo",
                 "org_alfresco_device_sync_repo",
-                "org_alfresco_mm_repo", "alfresco-ai-repo",
+                "org_alfresco_mm_repo",
+                "alfresco-ai-repo",
                 "org_alfresco_module_rm", "alfresco-rm-enterprise-repo",
-                "alfresco-glacier-connector-repo"
+                "alfresco-glacier-connector-repo",
                 // TODO uncomment this amp once https://issues.alfresco.com/jira/browse/MNT-21648 is done
                 //"org.alfresco.module.TransformationServer"
         );
