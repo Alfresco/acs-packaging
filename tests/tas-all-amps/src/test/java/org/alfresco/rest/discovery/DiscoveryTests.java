99c7183a0c7e8c430e2dd737538bbdf2b6cf619e
cturlica <cristian.turlica@ness.com>
2020-06-29 10:06:44 +0300

    ACS-370: Newer metadata-extractor

--- tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
+++ tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
@@ -76,7 +76,7 @@ public class DiscoveryTests extends RestTest
                 "alfresco-share-services",
                 "alfresco-saml-repo",
                 "org_alfresco_device_sync_repo",
-                //"org_alfresco_mm_repo", 
+                "org_alfresco_mm_repo", 
                 "alfresco-ai-repo",
                 "alfresco-glacier-connector-repo",
                 "org.alfresco.module.TransformationServer");
