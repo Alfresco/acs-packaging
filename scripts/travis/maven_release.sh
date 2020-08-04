6ab090c1117984787950630e2ac754b92dba1ecf
Cezar.Leahu <cezar.leahu@ness.com>
2020-08-04 16:19:35 +0300

    ACS-457: Build linkage, tagging and release
    - update release scripts

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 set -ev
 
+RELEASE_VERSION=${1}
+DEVELOPMENT_VERSION=${2}
+
+if [ -z "${RELEASE_VERSION}" ] || [ -z "${DEVELOPMENT_VERSION}" ]; then
+  echo "Please provide a Release and Development version in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
+  exit 1
+fi
+
 # Use full history for release
 git checkout -B "${TRAVIS_BRANCH}"
 # Add email to link commits to user
 git config user.email "${GIT_EMAIL}"
 
-if [ -z "${RELEASE_VERSION}" ] || [ -z "${DEVELOPMENT_VERSION}" ]; then
-  echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
-  exit 1
-fi
-
 mvn -B \
-  -PfullBuild,all-tas-tests \
-  -Prelease \
-  -DskipTests \
-  -Dusername="${GIT_USERNAME}" \
-  -Dpassword="${GIT_PASSWORD}" \
+  -Prelease,fullBuild,all-tas-tests \
   -DreleaseVersion="${RELEASE_VERSION}" \
   -DdevelopmentVersion="${DEVELOPMENT_VERSION}" \
-  -Dbuild-number="${TRAVIS_BUILD_NUMBER}" \
-  -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
-  -Dversion.edition="Enterprise" \
+  "-Darguments=-Prelease,fullBuild,all-tas-tests -DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER}" \
+  release:clean release:prepare release:perform \
   -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
-  "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -PfullBuild,all-tas-tests" \
-  release:clean release:prepare release:perform
+  -Dusername="${GIT_USERNAME}" \
+  -Dpassword="${GIT_PASSWORD}"
 
