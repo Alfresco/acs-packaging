889c786ecf253cbb0f1c4a43a24aa1faa6cdbe6f
Abdul Mohammed <abdul.mohammed@alfresco.com>
2020-08-05 17:22:19 +0100

    MNT-21614: Bump alfresco-repository version and add a test

--- tests/tas-integration/src/test/java/org/alfresco/tas/integration/subsystems/ldap/LdapUserSynchronizationTest.java
+++ tests/tas-integration/src/test/java/org/alfresco/tas/integration/subsystems/ldap/LdapUserSynchronizationTest.java
@@ -58,4 +58,45 @@ public class LdapUserSynchronizationTest extends IntegrationTest
                     String.format("The ldap user sync job didn't execute.\nPrevious run time: [%s]\nNew run time: [%s]", syncStatusPrevExecutionTime, syncStatusNewExecutionTime));
         });
     }
+
+    @Test(groups = {TestGroup.LDAP})
+    @TestRail(section = {TestGroup.LDAP}, executionType = ExecutionType.SANITY, description = "Verify exceptions java.naming.* when communication breaksdown between ldap server and ACS")
+    public void triggerSyncJobAndCheckStatusForException() throws Exception
+    {
+        String ldapUrl = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url").toString();
+
+        STEP("1. Change the ldap port to something non-existent by appending a 0 to ldap port -> causes the subsystem to restart");
+        jmxBuilder.getJmxClient().writeProperty("Alfresco:Category=Authentication,Type=Configuration,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url", ldapUrl + "0");
+
+        // Wait for Synchronization (LDAP) subsystem to restart
+        Utility.sleep(500, 5000, () ->
+        {
+            String ldapEnabled = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.active").toString();
+            Assert.assertEquals(ldapEnabled, Boolean.TRUE.toString(), String.format("Property ldap.authentication.active is [%s]", ldapEnabled));
+        });
+
+        STEP("2. Trigger the ldap users sync job");
+        jmxBuilder.getJmxClient().executeJMXMethod("Alfresco:Name=Schedule,Group=DEFAULT,Type=MonitoredCronTrigger,Trigger=syncTrigger", "executeNow");
+
+        // Wait for the sync job to finish
+        Utility.sleep(300, 5000, () ->
+        {
+            STEP("3. Verify the exception found in communication breakdown");
+            String syncStatusLastErrorMessage = jmxBuilder.getJmxClient().readProperty("Alfresco:Name=BatchJobs,Type=Synchronization,Category=manager", "LastErrorMessage").toString();
+            String expectedException = "javax.naming.CommunicationException";
+            Assert.assertEquals(syncStatusLastErrorMessage.contains(expectedException), Boolean.TRUE.booleanValue());
+        });
+
+        STEP("4. Revert back the ldap port to it's original value-> causes the subsystem to restart");
+        jmxBuilder.getJmxClient().writeProperty("Alfresco:Category=Authentication,Type=Configuration,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url", ldapUrl);
+
+        // Wait for Synchronization (LDAP) subsystem to restart
+        Utility.sleep(500, 5000, () ->
+        {
+            String ldapUrlCheck = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url").toString();
+            String ldapEnabled = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.active").toString();
+            Assert.assertEquals(ldapUrl, ldapUrlCheck, String.format("Property ldap.authentication.java.naming.provider.url is [%s]", ldapUrl));
+            Assert.assertEquals(ldapEnabled, Boolean.TRUE.toString(), String.format("Property ldap.authentication.active is [%s]", ldapEnabled));
+        });
+    }
 }
