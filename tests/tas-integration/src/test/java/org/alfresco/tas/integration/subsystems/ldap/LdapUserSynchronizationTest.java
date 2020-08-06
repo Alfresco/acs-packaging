1fa79f2ff1fd19648c579c0ebb79a6f71cc36958
Abdul Mohammed <abdul.mohammed@alfresco.com>
2020-08-06 12:54:59 +0100

    MNT-21614: LDAP sync communication error leads to group members deletion (#1551)
    * MNT-21614: Bump alfresco-repository version and add a test

--- tests/tas-integration/src/test/java/org/alfresco/tas/integration/subsystems/ldap/LdapUserSynchronizationTest.java
+++ tests/tas-integration/src/test/java/org/alfresco/tas/integration/subsystems/ldap/LdapUserSynchronizationTest.java
@@ -58,4 +58,49 @@ public class LdapUserSynchronizationTest extends IntegrationTest
                     String.format("The ldap user sync job didn't execute.\nPrevious run time: [%s]\nNew run time: [%s]", syncStatusPrevExecutionTime, syncStatusNewExecutionTime));
         });
     }
+
+    @Test(groups = {TestGroup.LDAP})
+    @TestRail(section = {TestGroup.LDAP}, executionType = ExecutionType.SANITY, description = "Verify exceptions java.naming.* when communication breaksdown between ldap server and ACS")
+    public void triggerSyncJobAndCheckStatusForException() throws Exception
+    {
+        String ldapUrl = "ldap://authentication:389";
+
+        STEP("1. Check ldap url is as expected");
+        String checkUrl = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url").toString();
+        Assert.assertEquals(checkUrl, ldapUrl);
+
+        STEP("2. Change the ldap port to something non-existent by appending a 0 to ldap port -> causes the subsystem to restart");
+        jmxBuilder.getJmxClient().writeProperty("Alfresco:Category=Authentication,Type=Configuration,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url", ldapUrl + "0");
+
+        // Wait for Synchronization (LDAP) subsystem to restart
+        Utility.sleep(500, 5000, () ->
+        {
+            String ldapEnabled = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.active").toString();
+            Assert.assertEquals(ldapEnabled, Boolean.TRUE.toString(), String.format("Property ldap.authentication.active is [%s]", ldapEnabled));
+        });
+
+        STEP("3. Trigger the ldap users sync job");
+        jmxBuilder.getJmxClient().executeJMXMethod("Alfresco:Name=Schedule,Group=DEFAULT,Type=MonitoredCronTrigger,Trigger=syncTrigger", "executeNow");
+
+        // Wait for the sync job to finish
+        Utility.sleep(300, 5000, () ->
+        {
+            STEP("4. Verify the exception found in communication breakdown");
+            String syncStatusLastErrorMessage = jmxBuilder.getJmxClient().readProperty("Alfresco:Name=BatchJobs,Type=Synchronization,Category=manager", "LastErrorMessage").toString();
+            String expectedException = "javax.naming.CommunicationException";
+            Assert.assertEquals(syncStatusLastErrorMessage.contains(expectedException), Boolean.TRUE.booleanValue());
+        });
+
+        STEP("5. Revert back the ldap port to it's original value and double check it is reverted -> causes the subsystem to restart");
+        jmxBuilder.getJmxClient().writeProperty("Alfresco:Category=Authentication,Type=Configuration,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url", ldapUrl);
+
+        // Wait for Synchronization (LDAP) subsystem to restart
+        Utility.sleep(500, 5000, () ->
+        {
+            String ldapUrlCheck = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.java.naming.provider.url").toString();
+            String ldapEnabled = jmxBuilder.getJmxClient().readProperty("Alfresco:Type=Configuration,Category=Authentication,id1=managed,id2=ldap1", "ldap.authentication.active").toString();
+            Assert.assertEquals(ldapUrl, ldapUrlCheck, String.format("Property ldap.authentication.java.naming.provider.url is [%s]", ldapUrl));
+            Assert.assertEquals(ldapEnabled, Boolean.TRUE.toString(), String.format("Property ldap.authentication.active is [%s]", ldapEnabled));
+        });
+    }
 }
