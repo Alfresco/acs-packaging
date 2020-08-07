c521269d1895759cf90ff31d12fad9ffffafadff
Cezar.Leahu <cezar.leahu@ness.com>
2020-08-07 14:48:18 +0300

    ACS-457: Build linkage, tagging and release
    - setup/update the S3 publishing jobs

--- scripts/veracode/static_analysis.sh
+++ scripts/veracode/static_analysis.sh
@@ -2,10 +2,16 @@
 set -ev
 echo "=========================== Starting Static Analysis Script ==========================="
 
-java -jar vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar -vid $VERACODE_API_ID -vkey $VERACODE_API_KEY \
-          -action uploadandscan -appname "ACS Repository" -sandboxname "${VERACODE_SANDBOX}" -createprofile false \
-           -filepath war/target/alfresco.war -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER" \
-|| (sleep 5m; exit 1) # wait 5 minutes before retry
+java -jar vosp-api-wrappers-java-${VERACODE_WRAPPER_VERSION}.jar \
+  -vid "${VERACODE_API_ID}" \
+  -vkey "${VERACODE_API_KEY}" \
+  -action uploadandscan \
+  -appname "ACS Repository" \
+  -sandboxname "${VERACODE_SANDBOX}" \
+  -createprofile false \
+  -filepath war/target/alfresco.war \
+  -version "${TRAVIS_JOB_ID} - ${TRAVIS_JOB_NUMBER}" \
+  || (sleep 5m; exit 1) # wait 5 minutes before retry
 
 
 echo "=========================== Finishing Static Analysis Script =========================="
