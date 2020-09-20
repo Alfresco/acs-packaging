c646364853bf44991498b768ee64a77aaebe7a6c
Alan Davis <alan.davis@alfresco.com>
2020-09-20 15:04:06 +0100

    Merge release changes from feature/REPO-5271_700

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -ev
+echo "=========================== Starting Build Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
 source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
@@ -19,21 +21,44 @@ if [[ $(isPullRequestBuild) && ( "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ||
   exit 1
 fi
 
+# Prevent release jobs from starting when there are SNAPSHOT upstream dependencies
+if [[ "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ || "${ENT_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] ; then
+  printf "Cannot release project with SNAPSHOT dependencies!\n"
+  exit 1
+fi
 
 COM_UPSTREAM_REPO="github.com/Alfresco/alfresco-community-repo.git"
+
+# For release jobs, check if the upstream dependency is the latest tag on the upstream repository (on the same branch)
+# TODO UNCOMMENT THIS LINE: if isBranchBuild && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && [ "${COM_DEPENDENCY_VERSION}" != "$(retieveLatestTag "${COM_UPSTREAM_REPO}" "${TRAVIS_BRANCH}")" ] ; then
+if isBranchBuild && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && [ "${COM_DEPENDENCY_VERSION}" != "$(retieveLatestTag "${COM_UPSTREAM_REPO}" "master")" ] ; then
+  printf "Upstream dependency is not up to date with %s / %s\n" "${COM_UPSTREAM_REPO}" "${TRAVIS_BRANCH}"
+  exit 1
+fi
+
 # Search, checkout and build the same branch on the upstream project in case of SNAPSHOT dependencies
 # Otherwise just checkout the upstream dependency sources
 if [[ "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] ; then
-  pullAndBuildSameBranchOnUpstream "${COM_UPSTREAM_REPO}" "-PcommunityDocker"
+  pullAndBuildSameBranchOnUpstream "${COM_UPSTREAM_REPO}" "-Pcommunity"
 else
   pullUpstreamTag "${COM_UPSTREAM_REPO}" "${COM_DEPENDENCY_VERSION}"
 fi
 
 ENT_UPSTREAM_REPO="github.com/Alfresco/alfresco-enterprise-repo.git"
+
+# For release jobs, check if the upstream dependency is the latest tag on the upstream repository (on the same branch)
+# TODO UNCOMMENT THIS LINE: if isBranchBuild && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && [ "${ENT_DEPENDENCY_VERSION}" != "$(retieveLatestTag "${ENT_UPSTREAM_REPO}" "${TRAVIS_BRANCH}")" ] ; then
+if isBranchBuild && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && [ "${ENT_DEPENDENCY_VERSION}" != "$(retieveLatestTag "${ENT_UPSTREAM_REPO}" "master")" ] ; then
+  printf "Upstream dependency is not up to date with %s / %s\n" "${ENT_UPSTREAM_REPO}" "${TRAVIS_BRANCH}"
+  exit 1
+fi
+
 # Search, checkout and build the same branch on the upstream project in case of SNAPSHOT dependencies
+# Otherwise, checkout the upstream tag and build its Docker image (use just "mvn package", without "mvn install")
 if [[ "${ENT_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] ; then
-  pullAndBuildSameBranchOnUpstream "${ENT_UPSTREAM_REPO}" \
-    "-PenterpriseDocker $([[ "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] && echo "-Dupstream.image.tag=latest")"
+  pullAndBuildSameBranchOnUpstream "${ENT_UPSTREAM_REPO}" "-PenterpriseDocker"
+else
+  pullUpstreamTagAndBuildDockerImage "${ENT_UPSTREAM_REPO}" "${ENT_DEPENDENCY_VERSION}" "-PenterpriseDocker"
 fi
 
 # Either both the parent and the upstream dependency are the same, or else fail the build
@@ -43,7 +68,11 @@ if [ "${COM_DEPENDENCY_VERSION}" != "$(evaluatePomProperty "project.parent.paren
 fi
 
 # Build the current project
-mvn -B -V -q install -DskipTests -PenterpriseDocker \
+mvn -B -V -q install -DskipTests -Dmaven.javadoc.skip=true -PenterpriseDocker \
   $([[ "${ENT_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] && echo "-Dupstream.image.tag=latest")
 
+
+popd
+set +vex
+echo "=========================== Finishing Build Script =========================="
 
