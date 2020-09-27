8a05242019c5d523cc0b3df4efbfaf2bcb25090a
Alan Davis <alan.davis@alfresco.com>
2020-09-27 08:33:51 +0100

    all amp module install test

--- tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
+++ tests/tas-all-amps/src/test/java/org/alfresco/rest/discovery/DiscoveryTests.java
@@ -79,7 +79,7 @@ public class DiscoveryTests extends RestTest
 //              "alfresco-ai-repo",
                 "alfresco-share-services",
 //                "org_alfresco_integrations_AzureConnector",
-//                "org_alfresco_mm_repo",
+                "org_alfresco_mm_repo",
 //              "alfresco-trashcan-cleaner",
                 "alfresco-content-connector-for-salesforce-repo");
 
@@ -87,6 +87,17 @@ public class DiscoveryTests extends RestTest
         List<String> modulesStates = restClient.onResponse().getResponse().jsonPath().getList("entry.repository.modules.installState", String.class);
         if (expectedModules.size() != Collections.frequency(modulesStates, "INSTALLED"))
         {
+            //([[installState:INSTALLED, versionMin:6.0, versionMax:999, installDate:2020-09-26T23:36:27.631+0000, description:Allows applications that can talk to a SharePoint server to talk to your Alfresco installation, id:alfresco-aos-module, title:Alfresco Office Services Module, version:1.2.2],
+            // [installState:INSTALLED, versionMin:6.0, versionMax:6.99, installDate:2020-09-26T23:36:27.273+0000, description:The Repository piece of the Alfresco SAML Module, id:alfresco-saml-repo, title:Alfresco SAML Repository AMP Module, version:1.1.0],
+            // [installState:INSTALLED, versionMin:6.0.0, versionMax:6.99.99, installDate:2020-09-26T23:36:27.617+0000, description:The Repository side artifacts of the Alfresco / Google Docs Integration., id:org.alfresco.integrations.google.docs, title:Alfresco / Google Docs Integration, version:3.1.0],
+            // [installState:INSTALLED, versionMin:6.0, versionMax:999, installDate:2020-09-26T23:36:27.744+0000, description:Alfresco Content Connector for EMC Centera, id:org_alfresco_module_xamconnector, title:Centera Connector, version:2.2.0],
+            // [installState:INSTALLED, versionMin:6.0.0, versionMax:6.99.99, installDate:2020-09-26T23:36:27.546+0000, description:Alfresco Document Transformation Engine Module for Repository, id:org.alfresco.module.TransformationServer, title:Document Transformation Engine AMP for Repository, version:2018.221],
+            // [installState:INSTALLED, versionMin:5.1, versionMax:999, installDate:2020-09-26T23:36:27.802+0000, description:Module to be applied to alfresco.war, containing APIs for Alfresco Share, id:alfresco-share-services, title:Alfresco Share Services AMP, version:6.0.1],
+            // [installState:INSTALLED, versionMin:2.1, versionMax:999, installDate:2020-09-26T23:36:27.471+0000, description:Alfresco Kofax Integration, id:org.alfresco.module.KofaxAddon, title:Kofax Add-on, version:2.0.0],
+            // [installState:INSTALLED, versionMin:6.0.0, versionMax:6.99.99, installDate:2020-09-26T23:36:27.712+0000, description:Extensions in the Alfresco repository to provide media / digital asset management (DAM) features, id:org_alfresco_mm_repo, title:Alfresco Media Management Repository AMP, version:1.2.0],
+            // [installState:UNKNOWN, versionMin:0, versionMax:999, description:The Alfresco Trashcan Cleaner (Alfresco Module), id:alfresco-trashcan-cleaner, title:alfresco-trashcan-cleaner project, version:2.3],
+            // [installState:INSTALLED, versionMin:6.0.0, versionMax:6.99.99, installDate:2020-09-26T23:36:27.532+0000, description:Alfresco Repository artifacts needed for the Alfresco Content Connector for Salesforce Repository Amp, id:alfresco-content-connector-for-salesforce-repo, title:Alfresco Content Connector for Salesforce Repository AMP, version:2.1.0]])
+
             String mods = restClient.onResponse().getResponse().jsonPath().getString("entry.repository.modules");
             assertEquals("Number of amps installed ("+mods+") should match expected", expectedModules.size(),
                     Collections.frequency(modulesStates, "INSTALLED"));
