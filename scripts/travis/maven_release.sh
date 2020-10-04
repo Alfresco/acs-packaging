a12aaae5d66b802bb28f3f5c5eaa48cb9a1983ca
Alan Davis <alan.davis@alfresco.com>
2020-10-04 13:02:03 +0100

    REPO-5271 Backport new project structure to release/6.2.2 (#1645)
    Original HF version was: 6.2.2.2
    
    Uses alfresco-community-repo / alfresco-enterprise-repo project structure
        Simplify dependencies, which flow downstream rather than being overridden in packaging.
        Content repo base image separated from acs-packaging.
        Licenses (and default key store in 6.2.1, 6.2.3 & 7.0.0) moved upstream. Allows use in upstream images.
        TAS tests moved upstream, where they are content repository specific.
        TAS tests backported to 6.2.0, 6.1.1, 6.1.0, 6.0.1 which did not have TAS within the project.
        TAS tests have been backported to 6.0.0 but cannot run as TAS requires Java 11.
        TAS test images modified in <= 6.1.0 to run as root, like the main images at that time.

--- scripts/travis/maven_release.sh
+++ scripts/travis/maven_release.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
-set -e
+echo "=========================== Starting Release Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+
+if [ -z "${RELEASE_VERSION}" ] || [ -z "${DEVELOPMENT_VERSION}" ]; then
+  echo "Please provide a Release and Development version in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
+  exit 1
+fi
 
 # Use full history for release
 git checkout -B "${TRAVIS_BRANCH}"
 # Add email to link commits to user
 git config user.email "${GIT_EMAIL}"
 
-if [ -z ${RELEASE_VERSION} ] || [ -z ${DEVELOPMENT_VERSION} ];
-    then echo "Please provide a Release and Development verison in the format <acs-version>-<additional-info> (6.3.0-EA or 6.3.0-SNAPSHOT)"
-         exit -1
-else
-    mvn --batch-mode \
-    -PfullBuild,all-tas-tests \
-    -DskipTests \
-    -Dusername="${GIT_USERNAME}" \
-    -Dpassword="${GIT_PASSWORD}" \
-    -DreleaseVersion=${RELEASE_VERSION} \
-    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
-    -Dbuild-number=${TRAVIS_BUILD_NUMBER} \
-    -Dbuild-name="${TRAVIS_BUILD_STAGE_NAME}" \
-    -Dversion.edition="Enterprise" \
-    -DscmCommentPrefix="[maven-release-plugin][skip ci]" \
-    "-Darguments=-DskipTests -Dversion.edition=Enterprise -Dbuild-number=${TRAVIS_BUILD_NUMBER} '-Dbuild-name=${TRAVIS_BUILD_STAGE_NAME}' -PfullBuild,all-tas-tests" \
-    -Prelease release:clean release:prepare release:perform
-fi
+mvn -B \
+  -Prelease,fullBuild,all-tas-tests,pipeline \
+  -DreleaseVersion="${RELEASE_VERSION}" \
+  -DdevelopmentVersion="${DEVELOPMENT_VERSION}" \
+  "-Darguments=-Prelease,fullBuild,all-tas-tests,pipeline -DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER}" \
+  release:clean release:prepare release:perform \
+  -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+  -Dusername="${GIT_USERNAME}" \
+  -Dpassword="${GIT_PASSWORD}"
+
+
+popd
+set +vex
+echo "=========================== Finishing Release Script =========================="
+
