befff6d97110ba94d5bc8ffac638b57f84095643
Alan Davis <alan.davis@alfresco.com>
2020-10-04 10:05:18 +0100

    REPO-5271 Backport new project structure to release/6.0.0 (#1639)
    Original HF version was: 6.0.0.5
    
        Uses alfresco-community-repo / alfresco-enterprise-repo project structure
            Simplify dependencies, which flow downstream rather than being overridden in packaging.
            Content repo base image separated from acs-packaging.
            Licenses (and default key store in 6.2.1, 6.2.3 & 7.0.0) moved upstream. Allows use in upstream images.
            TAS tests moved upstream, where they are content repository specific.
            TAS tests backported to 6.2.0, 6.1.1, 6.1.0, 6.0.1 which did not have TAS within the project.
            TAS tests have been backported to 6.0.0 but cannot run as TAS requires Java 11.
            TAS test images modified in <= 6.1.0 to run as root, like the main images at that time.

---
+++ scripts/travis/maven_release.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Release Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+
+if [ -z "${RELEASE_VERSION}" ] || [ -z "${DEVELOPMENT_VERSION}" ]; then
+  echo "Please provide a Release and Development version in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
+  exit 1
+fi
+
+# Use full history for release
+git checkout -B "${TRAVIS_BRANCH}"
+# Add email to link commits to user
+git config user.email "${GIT_EMAIL}"
+
+mvn -B \
+  -Prelease,fullBuild,all-tas-tests,pipeline \
+  -DreleaseVersion="${RELEASE_VERSION}" \
+  -DdevelopmentVersion="${DEVELOPMENT_VERSION}" \
+  "-Darguments=-Prelease,fullBuild,all-tas-tests,pipeline -DskipTests -Dbuild-number=${TRAVIS_BUILD_NUMBER}" \
+  release:clean release:prepare release:perform \
+  -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+  -Dusername="${GIT_USERNAME}" \
+  -Dpassword="${GIT_PASSWORD}"
+
+
+popd
+set +vex
+echo "=========================== Finishing Release Script =========================="
+
---
+++ scripts/travis/verify_release_tag.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Verify Release Tag Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+#
+# Check that the version to be released does not already have a docker tag.
+#
+
+if [ -z "${RELEASE_VERSION}" ]; then
+  echo "Please provide a RELEASE_VERSION in the format <acs-version>-<additional-info> (7.0.0-EA or 7.0.0-SNAPSHOT)"
+  exit 1
+fi
+
+if git rev-parse "${RELEASE_VERSION}^{tag}" &>/dev/null ; then
+  echo "The RELEASE_VERSION tag \"${RELEASE_VERSION}\" already exists in the git project"
+  exit 1
+fi
+
+# get the image name from the pom file
+ALFRESCO_DOCKER_IMAGE="$(mvn -B -q help:evaluate -f ./docker-alfresco/pom.xml -Dexpression=image.name -DforceStdout)"
+DOCKER_IMAGE_FULL_NAME="${ALFRESCO_DOCKER_IMAGE}:${RELEASE_VERSION}"
+
+function docker_image_exists() {
+  local IMAGE_FULL_NAME="${1}"; shift
+  local WAIT_TIME="${1:-5}"
+  local SEARCH_TERM='Pulling|is up to date|not found'
+
+  echo "Looking to see if ${IMAGE_FULL_NAME} already exists..."
+  local RESULT=$( (timeout --preserve-status "${WAIT_TIME}" docker 2>&1 pull "${IMAGE_FULL_NAME}" &) | grep -v 'Pulling repository' | grep -E -o "${SEARCH_TERM}")
+
+  test "${RESULT}" || { echo "Timed out too soon. Try using a wait_time greater than ${WAIT_TIME}..."; return 1 ;}
+  if echo "${RESULT}" | grep -vq 'not found'; then
+      true
+  else
+      false
+  fi
+}
+
+if docker_image_exists "${DOCKER_IMAGE_FULL_NAME}" ; then
+    echo "Tag ${RELEASE_VERSION} already pushed, release process will interrupt."
+    exit 1
+else
+    echo "The ${RELEASE_VERSION} tag was not found"
+fi
+
+
+popd
+set +vex
+echo "=========================== Finishing Verify Release Tag Script =========================="
+
