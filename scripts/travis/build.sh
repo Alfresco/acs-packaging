8b2e00e6bc23728957227a297992d6a17f9da571
Cezar.Leahu <cezar.leahu@ness.com>
2021-01-14 14:01:54 +0200

    [skip tests] [release] 7.0.0-A21-CM2

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -28,36 +28,10 @@ if [[ "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ || "${ENT_DEPENDENCY_VERSION}
 fi
 
 COM_UPSTREAM_REPO="github.com/Alfresco/alfresco-community-repo.git"
-
-# For release jobs, check if the upstream dependency is the latest tag on the upstream repository (on the same branch)
-if isBranchBuild && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && [ "${COM_DEPENDENCY_VERSION}" != "$(retieveLatestTag "${COM_UPSTREAM_REPO}" "${TRAVIS_BRANCH}")" ] ; then
-  printf "Upstream dependency is not up to date with %s / %s\n" "${COM_UPSTREAM_REPO}" "${TRAVIS_BRANCH}"
-  exit 1
-fi
-
-# Search, checkout and build the same branch on the upstream project in case of SNAPSHOT dependencies
-# Otherwise just checkout the upstream dependency sources
-if [[ "${COM_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] ; then
-  pullAndBuildSameBranchOnUpstream "${COM_UPSTREAM_REPO}" "-Pcommunity"
-else
-  pullUpstreamTag "${COM_UPSTREAM_REPO}" "${COM_DEPENDENCY_VERSION}"
-fi
+pullUpstreamTag "${COM_UPSTREAM_REPO}" "${COM_DEPENDENCY_VERSION}"
 
 ENT_UPSTREAM_REPO="github.com/Alfresco/alfresco-enterprise-repo.git"
-
-# For release jobs, check if the upstream dependency is the latest tag on the upstream repository (on the same branch)
-if isBranchBuild && [ "${TRAVIS_BUILD_STAGE_NAME,,}" = "release" ] && [ "${ENT_DEPENDENCY_VERSION}" != "$(retieveLatestTag "${ENT_UPSTREAM_REPO}" "${TRAVIS_BRANCH}")" ] ; then
-  printf "Upstream dependency is not up to date with %s / %s\n" "${ENT_UPSTREAM_REPO}" "${TRAVIS_BRANCH}"
-  exit 1
-fi
-
-# Search, checkout and build the same branch on the upstream project in case of SNAPSHOT dependencies
-# Otherwise, checkout the upstream tag and build its Docker image (use just "mvn package", without "mvn install")
-if [[ "${ENT_DEPENDENCY_VERSION}" =~ ^.+-SNAPSHOT$ ]] ; then
-  pullAndBuildSameBranchOnUpstream "${ENT_UPSTREAM_REPO}" "-PenterpriseDocker"
-else
-  pullUpstreamTagAndBuildDockerImage "${ENT_UPSTREAM_REPO}" "${ENT_DEPENDENCY_VERSION}" "-PenterpriseDocker"
-fi
+pullUpstreamTagAndBuildDockerImage "${ENT_UPSTREAM_REPO}" "${ENT_DEPENDENCY_VERSION}" "-PenterpriseDocker"
 
 # Either both the parent and the upstream dependency are the same, or else fail the build
 if [ "${COM_DEPENDENCY_VERSION}" != "$(evaluatePomProperty "project.parent.parent.version")" ]; then
