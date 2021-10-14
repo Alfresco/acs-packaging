d24e7768bfe9e8539f7b744060d408e6e77edbbc
rrajoria <rahul.rajoria@globallogic.com>
2021-10-14 15:05:48 +0530

    ACS-2068 : Copy Image to Dockerhub

---
+++ scripts/travis/copyImage_to_dockerHub.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Copy Image To Dockerhub Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set +e -v -x
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+#
+# Copy Image from Quay to dockerhub
+#
+
+TAG_NAME=${RELEASE_VERSION}
+SOURCE_IMAGE=quay.io/alfresco/alfresco-share
+TARGET_IMAGE=docker.io/alfresco/alfresco-share
+
+echo "${QUAY_PASSWORD}" | docker login -u="${QUAY_USERNAME}" --password-stdin quay.io
+docker pull $SOURCE_IMAGE:$TAG_NAME
+docker tag $SOURCE_IMAGE:$TAG_NAME $TARGET_IMAGE:$TAG_NAME
+
+echo "${DOCKERHUB_PASSWORD}" | docker login -u="${DOCKERHUB_USERNAME}" --password-stdin docker.io
+docker push $TARGET_IMAGE:$TAG_NAME
+
+popd
+set +vex
+echo "=========================== Finishing Copy Image To Dockerhub Script =========================="
+
+exit ${SUCCESS}
