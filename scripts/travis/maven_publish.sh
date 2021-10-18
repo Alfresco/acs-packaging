7e0b0c062bd6e45f186920d7f7150a4c6782712f
alandavis <alan.davis@alfresco.com>
2021-10-18 19:05:08 +0100

    ACS-2164/ACS-2165 Internal artifacts
    * Correct the alfresco-internal url in pom
    * Next attempt at download and upload of distribution zips
    
    Test [release] 7.2.0-A3 [skip tests][skip docker_release][no downstream]

--- scripts/travis/maven_publish.sh
+++ scripts/travis/maven_publish.sh
@@ -18,42 +18,35 @@ function publishDistributionZip() {
   local VERSION="${3}"
 
   local ARTIFACT_PATH="$(echo ${GROUP_ID}/${ARTIFACT_ID} | sed 's/\./\//g')"
-  local LOCAL_PATH="~/.m2/repository/${ARTIFACT_PATH}"
+  local LOCAL_PATH="${HOME}/.m2/repository/${ARTIFACT_PATH}"
   local TMP_PATH="/tmp/${ARTIFACT_ID}"
-  local FILE_NAME=${ARTIFACT_ID}-${VERSION}
-
-  echo
-  echo "     GROUP_ID=${GROUP_ID}"
-  echo "  ARTIFACT_ID=${ARTIFACT_ID}"
-  echo "   LOCAL_PATH=${LOCAL_PATH}"
-  echo "ARTIFACT_PATH=${ARTIFACT_PATH}"
-  echo "   LOCAL_PATH=${LOCAL_PATH}"
-  echo "     TMP_PATH=${TMP_PATH}"
-  echo "    FILE_NAME=${FILE_NAME}"
 
   rm -rf "${LOCAL_PATH}"
   mvn org.apache.maven.plugins:maven-dependency-plugin:get  \
-    -Dartifact=${GROUP_ID}:${ARTIFACT_ID}:${VERSION}:${VERSION} \
+    -Dartifact=${GROUP_ID}:${ARTIFACT_ID}:${VERSION}:zip \
     -Dtransitive=false
   ls -l "${LOCAL_PATH}/${VERSION}"
 
   rm -rf "${TMP_PATH}"
   mv "${LOCAL_PATH}/${VERSION}" "${TMP_PATH}"
 
-  echo mvn deploy:deploy-file \
-       -Dfile="${TMP_PATH}/${FILE_NAME}.${TYPE}" \
-    -DpomFile="${TMP_PATH}/${FILE_NAME}.pom" \
-    -Dsources="${TMP_PATH}/${FILE_NAME}-sources.jar"\
-      -Dfiles="${TMP_PATH}/${FILE_NAME}.zip" \
-    -Dtypes=zip
+  mvn deploy:deploy-file \
+       -Dfile="${TMP_PATH}/${ARTIFACT_ID}-${VERSION}.zip" \
     -DrepositoryId=alfresco-enterprise-releases \
     -Durl=https://nexus.alfresco.com/nexus/content/repositories/enterprise-releases/ \
-    -DgroupId="${GROUP_ID}" -DartifactId="${ARTIFACT_ID}" -Dversion="${VERSION}"
+    -DgroupId="${GROUP_ID}" -DartifactId="${ARTIFACT_ID}" -Dversion="${VERSION}" \
+    -Dpackaging=zip
+#    -DpomFile="${TMP_PATH}/${ARTIFACT_ID}-${VERSION}.pom" \ - overwrites the zip
 }
 
-publishDistributionZip org.alfresco alfresco-content-services-distribution ${RELEASE_VERSION}
+publishDistributionZip org.alfresco alfresco-content-services-distribution               ${RELEASE_VERSION}
+publishDistributionZip org.alfresco alfresco-content-services-share-distribution         ${RELEASE_VERSION}
+publishDistributionZip org.alfresco alfresco-governance-services-enterprise-distribution ${RELEASE_VERSION}
 
 popd
 set +vex
 echo "=========================== Finishing Release Script =========================="
+
+# TODO remove: Exit for now with an error as we don't want to copy to the S3 RELEASE area which happens next.
+exit 123
 
