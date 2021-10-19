10dc2e0452218d5d7f6c92edd1a297b30a6df009
alandavis <alan.davis@alfresco.com>
2021-10-19 18:22:49 +0100

    ACS-2173 share.war missing from alfresco-content-services-community-distribution:7.1.0
    Try to copy alfresco-content-services-share-distribution into the Community 'releases' Nexus repo, as it was prior to 7.1.0, so that it is available to acs-community-packaging
    
    Trial [release] 7.2.0-A4 [skip tests][skip docker_latest]
    
    [publish] includes code to avoid the S3 STAGING copy to RELEASES (will fail with error 123)

--- scripts/travis/maven_publish.sh
+++ scripts/travis/maven_publish.sh
@@ -7,47 +7,22 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
+source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
+
 
 if [ -z "${RELEASE_VERSION}" ]; then
   echo "Please provide a Release version in the format <acs-version>-<additional-info> (7.2.0-EA or 7.2.0)"
   exit 1
 fi
 
-function publishDistributionZip() {
-  local GROUP_ID="${1}"
-  local ARTIFACT_ID="${2}"
-  local VERSION="${3}"
-
-  local ARTIFACT_PATH="$(echo ${GROUP_ID}/${ARTIFACT_ID} | sed 's/\./\//g')"
-  local LOCAL_PATH="${HOME}/.m2/repository/${ARTIFACT_PATH}"
-  local TMP_PATH="/tmp/${ARTIFACT_ID}"
-
-  # Download the artifact. Make sure we are not using a cached version
-  rm -rf "${LOCAL_PATH}"
-  mvn org.apache.maven.plugins:maven-dependency-plugin:get  \
-    -Dartifact=${GROUP_ID}:${ARTIFACT_ID}:${VERSION}:zip \
-    -Dtransitive=false
-  ls -l "${LOCAL_PATH}/${VERSION}"
-
-  # The local maven repo must not contain the downloaded artifacts otherwise the upload can fail
-  rm -rf "${TMP_PATH}"
-  mv "${LOCAL_PATH}/${VERSION}" "${TMP_PATH}"
-
-  # Upload the artifact
-  mvn deploy:deploy-file \
-       -Dfile="${TMP_PATH}/${ARTIFACT_ID}-${VERSION}.zip" \
-    -DrepositoryId=alfresco-enterprise-releases \
-    -Durl=https://nexus.alfresco.com/nexus/content/repositories/enterprise-releases/ \
-    -DgroupId="${GROUP_ID}" -DartifactId="${ARTIFACT_ID}" -Dversion="${VERSION}" \
-    -Dpackaging=zip
-}
-
-publishDistributionZip org.alfresco alfresco-content-services-distribution               ${RELEASE_VERSION}
-publishDistributionZip org.alfresco alfresco-content-services-share-distribution         ${RELEASE_VERSION}
-publishDistributionZip org.alfresco alfresco-governance-services-enterprise-distribution ${RELEASE_VERSION}
+publishDistributionZip org.alfresco alfresco-content-services-distribution               ${RELEASE_VERSION} https://nexus.alfresco.com/nexus/content/repositories/enterprise-releases/
+publishDistributionZip org.alfresco alfresco-governance-services-enterprise-distribution ${RELEASE_VERSION} https://nexus.alfresco.com/nexus/content/repositories/enterprise-releases/
 
 popd
 set +vex
 echo "=========================== Finishing Release Script =========================="
+
+echo STOP BEFORE WE COPY S3 STAGING TO S3 RELEASES. WHEN WORKING REMOVE THIS echo AND exit.
+exit 123
 
 
