b7a561d6c8457bd6b78413d621e921048272bb6d
Damian Ujma <damian.ujma@hyland.com>
2021-10-20 14:24:25 +0200

    ACS-2130 Fix source_clear.sh

--- scripts/travis/source_clear.sh
+++ scripts/travis/source_clear.sh
@@ -9,11 +9,11 @@ mvn -B -q clean install \
     -DskipTests \
     -Dmaven.javadoc.skip=true \
     com.srcclr:srcclr-maven-plugin:scan \
-    -Dcom.srcclr.apiToken=${VERACODE_FAILS_BUILD} > scan.log
+    -Dcom.srcclr.apiToken=${SRCCLR_API_TOKEN} > scan.log
 
 SUCCESS=$?   # this will read exit code of the previous command
 
-if [ -z "$SRCCLR_FAILS_BUILD" ] || [ "$SRCCLR_FAILS_BUILD" = false ] ; then
+if [ -z "$VERACODE_FAILS_BUILD" ] || [ "$VERACODE_FAILS_BUILD" = false ] ; then
     SUCCESS=0
 fi
 
