9dea4af7f67ed1a0dab3b8688bf88d59758897ef
alandavis <alan.davis@alfresco.com>
2021-10-20 17:14:45 +0100

    Internal nexus repository, missing share.war, linkPoms.sh and trigger acs-community-packaging
    Cherry pick from 7.1.N which in turn was from master
    (cherry picked from commit c67bfd2930e34ea83c4ec7612cbded801b6e399e)
    
    ---------
    Cherry picks from master (7.2.0):
    
    Addition of linkPoms.sh
    (cherry picked from commit a7cc244232d3b73444a28f71ad4db4f2876b58cc)
    
    ACS-2173 share.war missing from alfresco-content-services-community-distribution:7.1.0
    Remove code to avoid the S3 STAGING copy to RELEASES
    (cherry picked from commit 517ade72053f1936d997850433e85d7cda4e4a9a)
    
    ACS-2173 share.war missing from alfresco-content-services-community-distribution:7.1.0
    Try to copy alfresco-content-services-share-distribution into the Community 'releases' Nexus repo, as it was prior to 7.1.0, so that it is available to acs-community-packaging
    (cherry picked from commit 10dc2e0452218d5d7f6c92edd1a297b30a6df009)
    
    ACS-2009 Alpha and Milestone releases are stored in internal nexus repository (#2086)
    (cherry picked from commit 7745244b60db473cf3d763b7948f6812ec0e38a6)
    
    Dev tomcat env needs maven-war-plugin < 3.3.0
    (cherry picked from commit 9ea7d03778f48ffb287d5b5de5b4d7babb51107e)
    
    Clean up downstream commit message
    (cherry picked from commit 856d4913933b58497714834e92ffa6d7dc221aa1)
    
    [downstream] try that again without Mac syntax in sed -i
    (cherry picked from commit 8fb505c19524a8176383e2d3ffbf07f51fa2b019)
    
    Update acs-community-packaging properties on release of acs-packaging
    (cherry picked from commit 90af4c4bdeb828116e1550dd57295bdb52780fd7)
    
    Update acs-community-packaging properties on release of acs-packaging
    (cherry picked from commit d3a2038d7f671b55d99302c86ef694d45429d60b)

--- scripts/travis/build_functions.sh
+++ scripts/travis/build_functions.sh
@@ -190,4 +190,34 @@ function retieveLatestTag() {
   rm -rf "${LOCAL_PATH}"
 }
 
+function publishDistributionZip() {
+  local GROUP_ID="${1}"
+  local ARTIFACT_ID="${2}"
+  local VERSION="${3}"
+  local NEXUS_REPO="${4}"
+
+  local ARTIFACT_PATH="$(echo ${GROUP_ID}/${ARTIFACT_ID} | sed 's/\./\//g')"
+  local LOCAL_PATH="${HOME}/.m2/repository/${ARTIFACT_PATH}"
+  local TMP_PATH="/tmp/${ARTIFACT_ID}"
+
+  # Download the artifact. Make sure we are not using a cached version
+  rm -rf "${LOCAL_PATH}"
+  mvn org.apache.maven.plugins:maven-dependency-plugin:get  \
+    -Dartifact=${GROUP_ID}:${ARTIFACT_ID}:${VERSION}:zip \
+    -Dtransitive=false
+  ls -l "${LOCAL_PATH}/${VERSION}"
+
+  # The local maven repo must not contain the downloaded artifacts otherwise the upload can fail
+  rm -rf "${TMP_PATH}"
+  mv "${LOCAL_PATH}/${VERSION}" "${TMP_PATH}"
+
+  # Upload the artifact
+  mvn deploy:deploy-file \
+    -Dfile="${TMP_PATH}/${ARTIFACT_ID}-${VERSION}.zip" \
+    -DrepositoryId=alfresco-enterprise-releases \
+    -Durl="${NEXUS_REPO}" \
+    -DgroupId="${GROUP_ID}" -DartifactId="${ARTIFACT_ID}" -Dversion="${VERSION}" \
+    -Dpackaging=zip
+}
+
 set -vx
