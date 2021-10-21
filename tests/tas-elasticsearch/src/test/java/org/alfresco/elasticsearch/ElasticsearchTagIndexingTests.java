bfe18fe9dff0b87d788a4ba7fa1cb7e0cef97ac9
pzurek <Piotr.Zurek@hyland.com>
2021-10-21 13:19:05 +0200

    More tests

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchTagIndexingTests.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchTagIndexingTests.java
@@ -2,9 +2,12 @@ package org.alfresco.elasticsearch;
 
 import static org.alfresco.elasticsearch.SearchQueryService.req;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.alfresco.rest.core.RestWrapper;
 import org.alfresco.rest.search.SearchRequest;
@@ -31,6 +34,8 @@ import org.testng.annotations.Test;
         initializers = AlfrescoStackInitializer.class)
 public class ElasticsearchTagIndexingTests extends AbstractTestNGSpringContextTests
 {
+    private static final Iterable<String> LANGUAGES_TO_CHECK = List.of("afts", "lucene");
+
     @Autowired
     ServerHealth serverHealth;
 
@@ -66,49 +71,122 @@ public class ElasticsearchTagIndexingTests extends AbstractTestNGSpringContextTe
     }
 
     @TestRail (section = { TestGroup.SEARCH, TestGroup.TAGS }, executionType = ExecutionType.REGRESSION,
-            description = "Verify the TAG query doesn't fail on unknown TAG")
+            description = "Verify the TAG queries work correctly")
     @Test (groups = { TestGroup.SEARCH, TestGroup.TAGS, TestGroup.REGRESSION })
-    public void shouldReturnEmptyResultForUnknownTag()
+    public void testSingleTagSearch()
     {
-        SearchRequest query = req("TAG:unknown");
-        searchQueryService.expectNoResultsFromQuery(query, testUser);
+        final String tag1 = unique("TAG1");
+
+        //No result for not existing tag
+        assertTagQueryResult(tag1, List.of());
+
+        //Tag first file - tag is created - expect single result
+        final FileModel file1 = givenFile("test1");
+        tagContent(file1, tag1);
+        assertTagQueryResult(tag1, List.of(file1));
+
+        //Tag another file - tag is reused - expect two items in the result
+        final FileModel file2 = givenFile("test2");
+        tagContent(file2, tag1);
+        assertTagQueryResult(tag1, List.of(file1, file2));
+
+        final String tag2 = unique("TAG2");
+
+        //Just a sanity check - No result for the second tag
+        assertTagQueryResult(tag2, List.of());
+
+        //Tag second file with a new tag
+        tagContent(file2, tag2);
+        assertTagQueryResult(tag1, List.of(file1, file2));
+        assertTagQueryResult(tag2, List.of(file2));
+
+        //Tag new file with the second tag
+        final FileModel file3 = givenFile("test3");
+        tagContent(file3, tag2);
+        assertTagQueryResult(tag1, List.of(file1, file2));
+        assertTagQueryResult(tag2, List.of(file2, file3));
+
+        //test disjunction and conjunction
+        assertTagQueryResult(tag1, "OR", tag2, List.of(file1, file2, file3));
+        assertTagQueryResult(tag1, "AND", tag2, List.of(file2));
+        final String unknownTag = unique("unknown");
+        assertTagQueryResult(tag1, "OR", unknownTag, List.of(file1, file2));
+        assertTagQueryResult(tag1, "AND", unknownTag, List.of());
     }
 
-    @TestRail (section = { TestGroup.SEARCH, TestGroup.TAGS }, executionType = ExecutionType.REGRESSION,
-            description = "Verify the TAG query doesn't fail on not used TAG")
-    @Test (groups = { TestGroup.SEARCH, TestGroup.TAGS, TestGroup.REGRESSION })
-    public void shouldReturnEmptyResultForExistingButNotUsedTag() throws Exception
+    private void assertTagQueryResult(String tag1Name, String operator, String tag2Name, Collection<ContentModel> contentModels)
     {
-        final FileModel testContent = givenContent("test");
-        final String tagName = unique("TAG");
+        final List<String> contentNames = contentModels
+                .stream()
+                .map(ContentModel::getName)
+                .collect(Collectors.toList());
 
-        tagContent(testContent, tagName);
-
-        assertTagQueryResult(tagName, testContent.getName());
-    }
-
-    private void assertTagQueryResult(String tagName, String ... names)
-    {
-        for (final String tag : List.of(tagName.toLowerCase(Locale.ROOT), tagName.toUpperCase(Locale.ROOT)))
+        for (final String tag1 : tagVariantsToCheck(tag1Name))
         {
-            for (final String language : List.of("afts", "lucene"))
+            for (final String tag2 : tagVariantsToCheck(tag2Name))
             {
-                Step.STEP("Searching for TAG `" + tag + "` using `" + language + "` language.");
-                searchQueryService.expectResultsFromQuery(req("TAG:" + tag), testUser, names);
+                for (final String language : LANGUAGES_TO_CHECK)
+                {
+                    Step.STEP("Searching for TAG `" + tag1 + "` " + operator + " `" + tag2 + "` using `" + language + "` language.");
+                    final SearchRequest query = req(language, "TAG:" + tag1 + " " + operator + " TAG:" + tag2);
+                    if (contentNames.isEmpty())
+                    {
+                        searchQueryService.expectNoResultsFromQuery(query, testUser);
+                    } else
+                    {
+                        Collections.shuffle(contentNames);
+                        searchQueryService.expectResultsFromQuery(query, testUser, contentNames.toArray(String[]::new));
+                    }
+                }
             }
         }
     }
 
-    private FileModel givenContent(final String content)
+    private void assertTagQueryResult(String tagName, Collection<ContentModel> contentModels)
     {
-        final FileModel file = new FileModel(unique("FILE"), FileType.TEXT_PLAIN, content);
+        final List<String> contentNames = contentModels
+                .stream()
+                .map(ContentModel::getName)
+                .collect(Collectors.toList());
+
+        for (final String tag : tagVariantsToCheck(tagName))
+        {
+            for (final String language : LANGUAGES_TO_CHECK)
+            {
+                Step.STEP("Searching for TAG `" + tag + "` using `" + language + "` language.");
+                final SearchRequest query = req(language, "TAG:" + tag);
+                if (contentNames.isEmpty())
+                {
+                    searchQueryService.expectNoResultsFromQuery(query, testUser);
+                } else
+                {
+                    Collections.shuffle(contentNames);
+                    searchQueryService.expectResultsFromQuery(query, testUser, contentNames.toArray(String[]::new));
+                }
+            }
+        }
+    }
+
+    private Iterable<String> tagVariantsToCheck(String tagName)
+    {
+        return List.of(
+                tagName.toLowerCase(Locale.ROOT),
+                tagName.toUpperCase(Locale.ROOT),
+                "\"" + tagName.toLowerCase(Locale.ROOT) + "\"",
+                "\"" + tagName.toUpperCase(Locale.ROOT) + "\""
+                      );
+    }
+
+    private FileModel givenFile(final String fileName)
+    {
+        final FileModel file = new FileModel(unique(fileName), FileType.TEXT_PLAIN, "Content for " + fileName);
         return dataContent
                 .usingAdmin()
                 .usingResource(testFolder)
                 .createContent(file);
     }
 
-    private void tagContent(ContentModel content, String ... tagNames)
+    private void tagContent(ContentModel content, String... tagNames)
     {
         final var contentData = dataContent
                 .usingAdmin()
