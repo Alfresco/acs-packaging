3cc9f79e1b03155e5717087a42f93dc504b4b9da
alandavis <alan.davis@alfresco.com>
2021-10-25 08:50:52 +0100

    linkPoms.sh: Faster version of extract [skip ci]

--- scripts/dev/linkPoms.sh
+++ scripts/dev/linkPoms.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="${SCRIPT_DIR}/../../.."
+ENV_FILE=".linkPoms.env"
+ENV_PATH="${ROOT_DIR}/${ENV_FILE}"
 
 source "$(dirname "${BASH_SOURCE[0]}")/dev_functions.sh"
 
@@ -57,19 +59,94 @@ function pull_latest() {
     fi
 }
 
-function exportPomProperty() {
-    local PROJECT="$1"
-    local ENV_NAME="$2"
-    local PROPERTY_NAME="$3"
+function readTopLevelTag() {
+  local TAG_NAME="${1}"
+  local POM_FILE="${2}"
+  # Might be possible to generalise this function to accept an XPath so it could be used in place of sed commands
 
-    if [ -d "${ROOT_DIR}/${PROJECT}" ]
-      then
-      pushd "${ROOT_DIR}/${PROJECT}" &>/dev/null
-      PROPERTY_VALUE=$(mvn help:evaluate -Dexpression="${PROPERTY_NAME}" -q -DforceStdout)
-      echo "export ${ENV_NAME}=${PROPERTY_VALUE}"
-      popd &>/dev/null
-      >&2 echo "${ENV_NAME}=${PROPERTY_VALUE}"
+  # Read the file with an IFS (Input Field Separator) of the start of XML tag character <
+  local IFS=\>
+  local DEPTH=-99
+  while read -d \< ENTITY CONTENT
+  do
+    if [[ $ENTITY == project\ * ]] # outer <project> tag
+    then
+      DEPTH=0
+    elif [[ $ENTITY == /* ]] # end tag
+    then
+      ((DEPTH=DEPTH-1))
+    else                     # start tag
+      ((DEPTH=DEPTH+1))
     fi
+
+    if [[ $ENTITY = "${TAG_NAME}" ]] && [[ $DEPTH == 1 ]] ; then
+        echo $CONTENT
+        exit
+    fi
+  done < $POM_FILE
+  exit 1
+}
+
+function exportPomVersion() {
+  local PROJECT="${1}"
+  local ENV_NAME="${2}"
+
+  if [ -d "${ROOT_DIR}/${PROJECT}" ]
+    then
+    pushd "${ROOT_DIR}/${PROJECT}" &>/dev/null
+    # Same as slower/simpler: "mvn help:evaluate -Dexpression=project.version"
+    VERSION=$(readTopLevelTag version pom.xml)
+    if [ $? -ne 0 ]
+    then
+      echo
+      echo "\"readTopLevelTagContent version pom.xml\" failed on ${PROJECT}"
+      exit 1
+    fi
+    echo "export ${ENV_NAME}=${VERSION}" >> "${ENV_PATH}"
+    popd &>/dev/null
+  fi
+}
+
+function exportPomParent() {
+  local PROJECT="${1}"
+  local ENV_NAME="${2}"
+
+  if [ -d "${ROOT_DIR}/${PROJECT}" ]
+    then
+    pushd "${ROOT_DIR}/${PROJECT}" &>/dev/null
+    # Same as slower/simpler: "mvn help:evaluate -Dexpression=project.parent.version"
+    PROPERTY_VALUE=$(sed -n '/<parent>/,/<\/parent>/p' pom.xml | sed -n "s/.*<version>\(.*\)<\/version>/\1/p")
+    if [ $? -ne 0 ]
+    then
+      echo
+      echo "sed -n '/<parent>/,/<\/parent>/p' pom.xml | sed -n \"s/.*<version>\(.*\)<\/version>/\1/p\" failed on ${PROJECT}"
+      exit 1
+    fi
+    echo "export ${ENV_NAME}=${PROPERTY_VALUE}" >> "${ENV_PATH}"
+    popd &>/dev/null
+  fi
+}
+
+# Original version was simpler/slower: exportPomPropertyOrig <project> <env_name> project.parent.version
+function exportPomProperty() {
+    local PROJECT="${1}"
+    local ENV_NAME="${2}"
+    local PROPERTY_NAME="${3}"
+
+  if [ -d "${ROOT_DIR}/${PROJECT}" ]
+    then
+    pushd "${ROOT_DIR}/${PROJECT}" &>/dev/null
+    # Same as slower/simpler: "mvn help:evaluate -Dexpression=${PROPERTY_NAME}"
+    PROPERTY_VALUE=$(sed -n '/<properties>/,/<\/properties>/p' pom.xml | sed -n "s/.*<${PROPERTY_NAME}>\(.*\)<\/${PROPERTY_NAME}>/\1/p")
+    if [ $? -ne 0 ]
+    then
+      echo
+      echo "sed -n '/<properties>/,/<\/properties>/p' pom.xml | sed -n \"s/.*<${PROPERTY_NAME}>\(.*\)<\/${PROPERTY_NAME}>/\1/p\" failed on ${PROJECT}"
+      exit 1
+    fi
+    echo "export ${ENV_NAME}=${PROPERTY_VALUE}" >> "${ENV_PATH}"
+    popd &>/dev/null
+  fi
 }
 
 while getopts "b:mpxuh" arg; do
@@ -136,29 +213,38 @@ done
 
 if [ -z ${SKIP_EXPORT+x} ]
 then
-  exportPomProperty alfresco-community-repo   COM_R_VERSION   project.version                               > .pomLink.env
+  rm -f "${ENV_FILE}"
 
-  exportPomProperty alfresco-enterprise-repo  ENT_R_VERSION   project.version                              >> .pomLink.env
-  exportPomProperty alfresco-enterprise-repo  ENT_R_PARENT    project.parent.version                       >> .pomLink.env
-  exportPomProperty alfresco-enterprise-repo  ENT_R_DEP_COM_R dependency.alfresco-community-repo.version   >> .pomLink.env
+  exportPomVersion  alfresco-community-repo   COM_R_VERSION
 
-  exportPomProperty alfresco-enterprise-share ENT_S_VERSION   project.version                              >> .pomLink.env
-  exportPomProperty alfresco-enterprise-share ENT_S_DEP_COM_R dependency.alfresco-community-repo.version   >> .pomLink.env
-  exportPomProperty alfresco-enterprise-share ENT_S_DEP_ENT_R dependency.alfresco-enterprise-repo.version  >> .pomLink.env
+  exportPomVersion  alfresco-enterprise-repo  ENT_R_VERSION
+  exportPomParent   alfresco-enterprise-repo  ENT_R_PARENT
+  exportPomProperty alfresco-enterprise-repo  ENT_R_DEP_COM_R dependency.alfresco-community-repo.version
 
-  exportPomProperty acs-packaging             ENT_P_PARENT    project.parent.version                       >> .pomLink.env
-  exportPomProperty acs-packaging             ENT_P_DEP_ENT_R dependency.alfresco-enterprise-repo.version  >> .pomLink.env
-  exportPomProperty acs-packaging             ENT_P_DEP_ENT_S dependency.alfresco-enterprise-share.version >> .pomLink.env
+  exportPomVersion  alfresco-enterprise-share ENT_S_VERSION
+  exportPomProperty alfresco-enterprise-share ENT_S_DEP_COM_R dependency.alfresco-community-repo.version
+  exportPomProperty alfresco-enterprise-share ENT_S_DEP_ENT_R dependency.alfresco-enterprise-repo.version
 
-  exportPomProperty acs-community-packaging   COM_P_PARENT    project.parent.version                       >> .pomLink.env
-  exportPomProperty acs-community-packaging   COM_P_DEP_COM_R dependency.alfresco-community-repo.version   >> .pomLink.env
-  exportPomProperty acs-community-packaging   COM_P_DEP_COM_S dependency.alfresco-community-share.version  >> .pomLink.env
-  echo
+  exportPomParent   acs-packaging             ENT_P_PARENT
+  exportPomProperty acs-packaging             ENT_P_DEP_ENT_R dependency.alfresco-enterprise-repo.version
+  exportPomProperty acs-packaging             ENT_P_DEP_ENT_S dependency.alfresco-enterprise-share.version
+
+  exportPomParent   acs-community-packaging   COM_P_PARENT
+  exportPomProperty acs-community-packaging   COM_P_DEP_COM_R dependency.alfresco-community-repo.version
+  exportPomProperty acs-community-packaging   COM_P_DEP_COM_S dependency.alfresco-community-share.version
+
+  cat "${ENV_FILE}"
 fi
 
 if [ -z ${SKIP_UPDATE+x} ]
 then
-  source .pomLink.env
+  if [ ! -f "${ENV_FILE}" ]
+  then
+    echo ""${ENV_FILE}" does not exist."
+    exit 1
+  fi
+
+  source "${ENV_FILE}"
 
   updatePomParent   alfresco-enterprise-repo  "$COM_R_VERSION"
   updatePomProperty alfresco-enterprise-repo  "$COM_R_VERSION" dependency.alfresco-community-repo.version
--- scripts/dev/unlinkPoms.sh
+++ scripts/dev/unlinkPoms.sh
@@ -3,11 +3,12 @@ set -o errexit
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="${SCRIPT_DIR}/../../.."
+ENV_FILE=".linkPoms.env"
 
 source "$(dirname "${BASH_SOURCE[0]}")/dev_functions.sh"
 
 usage() {
-    echo "Reverts changes made by linkPoms.sh using values stored in .pomLink.env" 1>&2;
+    echo "Reverts changes made by linkPoms.sh using values stored in ${ENV_FILE}" 1>&2;
     echo 1>&2;
     echo "Usage: $0 [-h]" 1>&2;
     echo "  -h: Display this help" 1>&2;
@@ -26,7 +27,7 @@ while getopts "lh" arg; do
     esac
 done
 
-source .pomLink.env
+source ${ENV_FILE}
 
 updatePomParent   alfresco-enterprise-repo  "$ENT_R_PARENT"
 updatePomProperty alfresco-enterprise-repo  "$ENT_R_DEP_COM_R" dependency.alfresco-community-repo.version
