678c9571421ed655f7915fd9f88dada834cd2ed8
alandavis <alan.davis@alfresco.com>
2021-11-04 11:08:45 +0000

    ACS-2224 Automatically update one pipeline flux config to reference ACS
    * [skip tests] as the code did not change

---
+++ scripts/travis/update_single_pipeline_reference.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Update Single Pipeline Reference ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+source "$(dirname "${BASH_SOURCE[0]}")/build_functions.sh"
+
+#Fetch the latest changes, as Travis will only checkout the PR commit
+git fetch origin "${TRAVIS_BRANCH}"
+git checkout "${TRAVIS_BRANCH}"
+git pull
+
+# Retrieve the ACS_VERSION version
+MAJOR="$(evaluatePomProperty "acs.version.major")"
+MINOR="$(evaluatePomProperty "acs.version.minor")"
+REVISION="$(evaluatePomProperty "acs.version.revision")"
+ACS_VERSION="${MAJOR}.${MINOR}.${REVISION}"
+
+DOWNSTREAM_REPO="github.com/Alfresco/terraform-alfresco-pipeline.git"
+
+cloneRepo "${DOWNSTREAM_REPO}" develop
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../$(basename "${DOWNSTREAM_REPO%.git}")"
+
+CONFIG_FILE=flux-configuration/develop/flux_configuration.yaml
+sed -i "s/\(.*repository: regex:\).*\((.*).*\)/\1$ACS_VERSION\2/" "${CONFIG_FILE}"
+sed -i "s/\(.*share: regex:\).*\((.*).*\)/\1$ACS_VERSION\2/" "${CONFIG_FILE}"
+
+# Commit changes
+git status
+git --no-pager diff "${CONFIG_FILE}"
+git add "${CONFIG_FILE}"
+
+if git status --untracked-files=no --porcelain | grep -q '^' ; then
+  git commit -m "Update upstream versions
+
+    - repository/share:   ${ACS_VERSION}"
+  git push
+else
+  echo "Dependencies are already up to date."
+  git status
+fi
+
+popd
+set +vex
+echo "=========================== Finishing Update Single Pipeline Reference =========================="
+
