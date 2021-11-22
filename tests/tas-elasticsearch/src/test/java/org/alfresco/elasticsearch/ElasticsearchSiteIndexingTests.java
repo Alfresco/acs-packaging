cb5eae2349768e1c5f5f806737410f03e7e802c2
Sara Aspery <sara.aspery@alfresco.com>
2021-11-22 07:19:54 +0000

    ACS-2240 add site create, modify, delete tests

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchSiteIndexingTests.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchSiteIndexingTests.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.alfresco.dataprep.AlfrescoHttpClientFactory;
+import org.alfresco.dataprep.SiteService.Visibility;
 import org.alfresco.rest.core.RestWrapper;
 import org.alfresco.rest.search.SearchRequest;
 import org.alfresco.utility.data.DataContent;
@@ -17,6 +17,7 @@ import org.alfresco.utility.data.DataUser;
 import org.alfresco.utility.model.ContentModel;
 import org.alfresco.utility.model.FileModel;
 import org.alfresco.utility.model.FileType;
+import org.alfresco.utility.model.FolderModel;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
 import org.alfresco.utility.model.UserModel;
@@ -36,8 +37,11 @@ public class ElasticsearchSiteIndexingTests extends AbstractTestNGSpringContextT
 {
     private static final Iterable<String> LANGUAGES_TO_CHECK = List.of("afts", "lucene");
     private static final FileModel DOCUMENT_LIBRARY = new FileModel("documentLibrary");
-    private static final String ALL_SITES = "_ALL_SITES_";
-    private static final String EVERYTHING = "_EVERYTHING_";
+    private static final String FILENAME_PREFIX = "EsSiteTest";
+    private static final String FILE_PREFIX_CONDITION = " cm:name:" + FILENAME_PREFIX + "*";
+    private static final String SAMPLE_SITE_ID = "swsdp";
+    private static final String ALL_SITES = "_ALL_SITES_ ";
+    private static final String EVERYTHING = "_EVERYTHING_ ";
 
     @Autowired
     ServerHealth serverHealth;
@@ -52,92 +56,253 @@ public class ElasticsearchSiteIndexingTests extends AbstractTestNGSpringContextT
     public DataSite dataSite;
 
     @Autowired
-    private AlfrescoHttpClientFactory alfrescoHttpClientFactory;
-
-    @Autowired
     SearchQueryService searchQueryService;
 
     @Autowired
     protected RestWrapper restClient;
 
     private UserModel testUser;
-    private SiteModel publicSite1;
+    private UserModel siteCreator;
+    private FolderModel testFolder;
 
     @BeforeClass (alwaysRun = true)
-    public void dataPreparation() throws Exception
+    public void dataPreparation()
     {
         serverHealth.assertServerIsOnline();
 
-        Step.STEP("Create a test user and public site.");
+        Step.STEP("Create test users and a folder.");
         testUser = dataUser.createRandomTestUser();
-        publicSite1 = dataSite.usingUser(testUser).createPublicRandomSite();
+        siteCreator = dataUser.createRandomTestUser();
+        testFolder = dataContent
+                .usingAdmin()
+                .usingResource(contentRoot())
+                .createFolder(new FolderModel(unique("FOLDER")));
     }
 
     @TestRail (section = { TestGroup.SEARCH, TestGroup.SITES }, executionType = ExecutionType.REGRESSION,
             description = "Verify the SITE queries work correctly")
     @Test (groups = { TestGroup.SEARCH, TestGroup.SITES, TestGroup.REGRESSION })
-    public void testSiteUseCases()
+    public void testSiteUseCasesForCreateModifyDeleteSite()
     {
-        Step.STEP("No such site exists so no results.");
+        // Remove the automatically created Sample Site
+        deleteSite(SAMPLE_SITE_ID);
+
+        Step.STEP("Site creation use cases");
+
+        // Check there are no files within or without a site, that have the given filename prefix
+        assertSiteQueryResult(ALL_SITES, List.of());
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of());
+
+        // Create a file with the given filename prefix, outside any site
+        FileModel fileNotInSite = new FileModel(unique(FILENAME_PREFIX) + ".txt", FileType.TEXT_PLAIN, "Content for fileNotInSite");
+        fileNotInSite = dataContent
+                .usingAdmin()
+                .usingResource(testFolder)
+                .createContent(fileNotInSite);
+
+        // Search with condition of filename prefix - we should see the file using EVERYTHING but not from ALL_SITES
+        assertSiteQueryResult(ALL_SITES, "AND", FILE_PREFIX_CONDITION, List.of());
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite));
+
+        // No sites should exist - expect no results
+        assertSiteQueryResult(ALL_SITES, List.of());
         assertSiteQueryResult(unique("NoSuchSite"), List.of());
 
-        Step.STEP("Public site has no files, so no results other than document library.");
-        assertSiteQueryResult(publicSite1.getId(), List.of(DOCUMENT_LIBRARY));
+        // Create one empty public site - expect no results other than document library
+        SiteModel testSite1 = createPublicSite();
+        assertSiteQueryResult(testSite1.getId(), List.of(DOCUMENT_LIBRARY));
+        assertSiteQueryResult(ALL_SITES, List.of(DOCUMENT_LIBRARY));
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite));
 
-        Step.STEP("Public site has one file, so expect one file plus the document library.");
-        FileModel file1 = createContentInSite(publicSite1, "test1");
-        assertSiteQueryResult(publicSite1.getId(), List.of(DOCUMENT_LIBRARY, file1));
+        // Create a file in public site - expect one file plus the document library.
+        FileModel file1 = createContentInSite(testSite1, FILENAME_PREFIX + "test1");
+        assertSiteQueryResult(testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1));
+        assertSiteQueryResult(ALL_SITES, List.of(DOCUMENT_LIBRARY, file1));
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1));
+
+        // Create another file in public site - expect two files plus the document library.
+        FileModel file2 = createContentInSite(testSite1, FILENAME_PREFIX + "test2");
+        assertSiteQueryResult(testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2));
+
+        // Create a second public site, empty - expect no results other than document library
+        SiteModel testSite2 = createPublicSite();
+        assertSiteQueryResult(testSite2.getId(), List.of(DOCUMENT_LIBRARY));
+        assertSiteQueryResult(testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2));
+
+        // Create a file in second public site - expect one file plus the document library.
+        FileModel file3 = createContentInSite(testSite2, FILENAME_PREFIX + "test3");
+        assertSiteQueryResult(testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3));
+        assertSiteQueryResult(testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2, file3));
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2, file3));
+
+        // Create another file in second public site - expect two files plus the document library.
+        FileModel file4 = createContentInSite(testSite2, FILENAME_PREFIX + "test4");
+        assertSiteQueryResult(testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2, file3, file4));
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2, file3, file4));
+
+        // Test disjunction and conjunction using SITE: and SITE:
+        assertSiteQueryResult(testSite1.getId(), "OR", " SITE:" + testSite2.getId(), List.of(DOCUMENT_LIBRARY, file1, file2, file3, file4));
+        assertSiteQueryResult(testSite1.getId(), "OR", " SITE:" + unique("NoSuchSite"), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(testSite1.getId(), "AND", " SITE:" + unique("NoSuchSite"), List.of());
+
+        // Test conjunction using SITE: and cm:name:
+        assertSiteQueryResult(testSite1.getId(), "AND", " cm:name:" + FILENAME_PREFIX + "test2*", List.of(file2));
+        assertSiteQueryResult(testSite1.getId(), "AND", " cm:name:" + FILENAME_PREFIX + "testX*", List.of());
+        assertSiteQueryResult(EVERYTHING, "AND", " cm:name:" + FILENAME_PREFIX + "test2*", List.of(file2));
+        assertSiteQueryResult(EVERYTHING, "AND", " cm:name:" + FILENAME_PREFIX + "testX*", List.of());
+        assertSiteQueryResult(ALL_SITES, "AND", " cm:name:" + FILENAME_PREFIX + "test2*", List.of(file2));
+        assertSiteQueryResult(ALL_SITES, "AND", " cm:name:" + FILENAME_PREFIX + "testX*", List.of());
+
+        // Test modify site
+        Step.STEP("Site modification use cases");
+
+        // Verify site creating user can see all files in both public sites
+        assertSiteQueryResult(siteCreator, testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(siteCreator, testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(siteCreator, ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2, file3, file4));
+        assertSiteQueryResult(siteCreator, EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2, file3, file4));
+
+        // Verify user who is not a member of any sites can see all files in both public sites
+        UserModel publicUser = dataUser.createRandomTestUser();
+        assertSiteQueryResult(publicUser, testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(publicUser, testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(publicUser, ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2, file3, file4));
+        assertSiteQueryResult(publicUser, EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2, file3, file4));
+
+        // Change first site's visibility to private - verify site creator can still see all files in both sites
+        dataSite.usingUser(siteCreator).updateSiteVisibility(testSite1, Visibility.PRIVATE);
+        assertSiteQueryResult(siteCreator, testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(siteCreator, testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(siteCreator, ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2, file3, file4));
+        assertSiteQueryResult(siteCreator, EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2, file3, file4));
+
+        // Verify user who is not a member of any sites can see files in public site but not see files in private site
+        assertSiteQueryResult(publicUser, testSite1.getId(), List.of());
+        assertSiteQueryResult(publicUser, testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(publicUser, ALL_SITES, List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(publicUser, EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file3, file4));
+
+        // Change site visibility back to public - user who is not a member of any sites can see files in both sites again
+        dataSite.usingUser(siteCreator).updateSiteVisibility(testSite1, Visibility.PUBLIC);
+        assertSiteQueryResult(publicUser, testSite1.getId(), List.of(DOCUMENT_LIBRARY, file1, file2));
+        assertSiteQueryResult(publicUser, testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(publicUser, ALL_SITES, List.of(DOCUMENT_LIBRARY, file1, file2, file3, file4));
+        assertSiteQueryResult(publicUser, EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file1, file2, file3, file4));
+
+        // Test delete site
+        Step.STEP("Site deletion use cases");
+
+        // Delete one site - expect no results for that site
+        deleteSite(testSite1.getId());
+        assertSiteQueryResult(testSite1.getId(), List.of());
+        assertSiteQueryResult(testSite2.getId(), List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(ALL_SITES, List.of(DOCUMENT_LIBRARY, file3, file4));
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite, file3, file4));
+
+        // Delete remaining site - expect no results
+        deleteSite(testSite2.getId());
+        assertSiteQueryResult(testSite2.getId(), List.of());
+        assertSiteQueryResult(testSite1.getId(), List.of());
+        assertSiteQueryResult(ALL_SITES, List.of());
+        assertSiteQueryResult(EVERYTHING, "AND", FILE_PREFIX_CONDITION, List.of(fileNotInSite));
     }
 
     private void assertSiteQueryResult(String siteName, Collection<ContentModel> contentModels)
+    {
+        assertSiteQueryResult(testUser, siteName, contentModels);
+    }
+
+    private void assertSiteQueryResult(UserModel user, String siteName, Collection<ContentModel> contentModels)
     {
         final List<String> contentNames = contentModels
                 .stream()
                 .map(ContentModel::getName)
                 .collect(Collectors.toList());
 
-            for (final String language : LANGUAGES_TO_CHECK)
+        for (final String language : LANGUAGES_TO_CHECK)
+        {
+            Step.STEP("Searching for SITE `" + siteName + "` using `" + language + "` language.");
+            final SearchRequest query = req(language, "SITE:" + siteName + " ");
+            if (contentNames.isEmpty())
             {
-                Step.STEP("Searching for SITE `" + siteName + "` using `" + language + "` language.");
-                final SearchRequest query = req(language, "SITE:" + siteName + " ");
-                if (contentNames.isEmpty())
-                {
-                    searchQueryService.expectNoResultsFromQuery(query, testUser);
-                }
-                else
-                {
-                    Collections.shuffle(contentNames);
-                    searchQueryService.expectResultsFromQuery(query, testUser, contentNames.toArray(String[]::new));
-                }
+                searchQueryService.expectNoResultsFromQuery(query, user);
             }
+            else
+            {
+                Collections.shuffle(contentNames);
+                searchQueryService.expectResultsFromQuery(query, user, contentNames.toArray(String[]::new));
+            }
+        }
+    }
+
+    private void assertSiteQueryResult(String site1Name, String operator, String condition, Collection<ContentModel> contentModels)
+    {
+        assertSiteQueryResult(testUser, site1Name, operator, condition, contentModels);
+    }
+
+    private void assertSiteQueryResult(UserModel user, String site1Name, String operator, String condition, Collection<ContentModel> contentModels)
+    {
+        final List<String> contentNames = contentModels
+                .stream()
+                .map(ContentModel::getName)
+                .collect(Collectors.toList());
+
+        for (final String language : LANGUAGES_TO_CHECK)
+        {
+            Step.STEP("Searching for SITE `" + site1Name + "` " + operator + " `" + condition + "` using `" + language + "` language.");
+//            final SearchRequest query = req(language, "SITE:" + site1Name + " "  + operator + condition);
+            final SearchRequest query = req("afts", "SITE:" + site1Name + " "  + operator + condition);
+            if (contentNames.isEmpty())
+            {
+                searchQueryService.expectNoResultsFromQuery(query, user);
+            }
+            else
+            {
+                Collections.shuffle(contentNames);
+                searchQueryService.expectResultsFromQuery(query, user, contentNames.toArray(String[]::new));
+            }
+        }
     }
 
     private FileModel createContentInSite(SiteModel site, String fileName)
     {
+        Step.STEP("Creating file '" + fileName + "' in site '" + site.getId() + "'.");
         final FileModel file = new FileModel(unique(fileName) + ".txt", FileType.TEXT_PLAIN, "Content for " + fileName);
-        return dataContent.usingUser(testUser)
-                .usingSite(site)
-                .createContent(file);
+        return dataContent.usingUser(siteCreator)
+                          .usingSite(site)
+                          .createContent(file);
     }
 
-    // will need this for additional tests
-//    private FileModel givenFile(final String fileName)
-//    {
-//        final FileModel file = new FileModel(unique(fileName), FileType.TEXT_PLAIN, "Content for " + fileName);
-//        return dataContent
-//                .usingAdmin()
-//                .usingResource(testFolder)
-//                .createContent(file);
-//    }
+    private SiteModel createPublicSite()
+    {
+        SiteModel createdSite = dataSite.usingUser(siteCreator).createPublicRandomSite();
+        Step.STEP("Created public site '" + createdSite.getId() + "'.");
+        return createdSite;
+    }
 
-    // will need this for additional tests
-//    private void deleteFile(ContentModel contentModel)
-//    {
-//        dataContent
-//                .usingAdmin()
-//                .usingResource(contentModel)
-//                .deleteContent();
-//    }
+    private void deleteSite(String siteId)
+    {
+        Step.STEP("Deleting site '" + siteId + "', if it exists");
+        SiteModel siteToDelete = new SiteModel(siteId);
+        if (dataSite.usingAdmin().isSiteCreated(siteToDelete))
+        {
+            dataSite.usingAdmin().deleteSite(siteToDelete);
+        }
+    }
+
+    private static ContentModel contentRoot()
+    {
+        final ContentModel root = new ContentModel("-root-");
+        root.setNodeRef(root.getName());
+        return root;
+    }
 
     private static String unique(String prefix)
     {
