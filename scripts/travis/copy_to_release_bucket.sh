1442c9c6abcf452cfc928bf61f5213dfc9633a60
Tom Page <tpage-alfresco@users.noreply.github.com>
2022-01-31 14:46:15 +0000

    ACS-2303 Ensure we copy the third party license report to the release bucket. (#2162)

--- scripts/travis/copy_to_release_bucket.sh
+++ scripts/travis/copy_to_release_bucket.sh
@@ -17,15 +17,7 @@ DESTINATION="s3://eu.dl.alfresco.com/release/enterprise/ACS/${RELEASE_VERSION:0:
 
 printf "\n%s\n%s\n" "${SOURCE}" "${DESTINATION}"
 
-aws s3 cp --acl private \
-  "${SOURCE}/alfresco.war" \
-  "${DESTINATION}/alfresco.war"
-
-aws s3 cp --acl private \
-  "${SOURCE}/alfresco-content-services-distribution-${RELEASE_VERSION}.zip" \
-  "${DESTINATION}/alfresco-content-services-distribution-${RELEASE_VERSION}.zip"
-
+aws s3 cp --acl private --recursive "${SOURCE}" "${DESTINATION}"
 
 set +vex
 echo "=========================== Finishing Copy to Release Bucket Script =========================="
-
