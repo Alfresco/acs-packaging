4424f0d12014b7885d17277dc175194e2604a3bf
Tom Page <thomas.page@alfresco.com>
2022-04-01 16:03:07 +0100

    ACS-2752 Tidy tests and add a couple more to prove querying is working against ES.

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchCMISTests.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchCMISTests.java
@@ -2,10 +2,8 @@ package org.alfresco.elasticsearch;
 
 import static org.alfresco.elasticsearch.SearchQueryService.req;
 
-import java.util.Map;
+import java.util.UUID;
 
-import org.alfresco.dataprep.AlfrescoHttpClient;
-import org.alfresco.dataprep.AlfrescoHttpClientFactory;
 import org.alfresco.rest.search.SearchRequest;
 import org.alfresco.utility.constants.UserRole;
 import org.alfresco.utility.data.DataContent;
@@ -19,9 +17,6 @@ import org.alfresco.utility.model.UserModel;
 import org.alfresco.utility.network.ServerHealth;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -36,10 +31,12 @@ import org.testng.annotations.Test;
  */
 public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
 {
-    private static final String FILE_0_NAME = "test.txt";
-    private static final String FILE_1_NAME = "another.txt";
-    private static final String FILE_2_NAME = "user1.txt";
-    private static final String FILE_3_NAME = "user1Old.txt";
+    private static final String PREFIX = UUID.randomUUID().toString().substring(0, 9);
+    private static final String FILE_0_NAME = PREFIX + "test.txt";
+    private static final String FILE_1_NAME = PREFIX + "another.txt";
+    private static final String FILE_2_NAME = PREFIX + "user1Old.txt";
+    /** This is a file that user 1 doesn't have access to and so shouldn't be returned in their search results. */
+    private static final String USER_2_FILE_NAME = PREFIX + "user2.txt";
 
     @Autowired
     private DataUser dataUser;
@@ -49,9 +46,6 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
 
     @Autowired
     private DataSite dataSite;
-
-    @Autowired
-    private AlfrescoHttpClientFactory alfrescoHttpClientFactory;
 
     @Autowired
     private ServerHealth serverHealth;
@@ -70,11 +64,11 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
      * <p>
      * Site1:
      * - Users: user1, userMultiSite
-     * - Documents: FILE_0_NAME (owner: user1), FILE_1_NAME (owner: user1), FILE_3_NAME (owner: user2)
+     * - Documents: FILE_0_NAME (owner: user1), FILE_1_NAME (owner: user1), FILE_2_NAME (owner: user2)
      * <p>
      * Site2:
      * - Users: user2, userMultiSite
-     * - Documents: FILE_2_NAME (owner: user2)
+     * - Documents: USER_2_FILE_NAME (owner: user2)
      */
     @BeforeClass(alwaysRun = true)
     public void dataPreparation()
@@ -94,15 +88,14 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
 
         createContent(FILE_0_NAME, "This is the first test", siteModel1, user1);
         createContent(FILE_1_NAME, "This is another TEST file", siteModel1, user1);
-        createContent(FILE_2_NAME, "This is another test file", siteModel2, user2);
-        createContent(FILE_3_NAME, "This is another Test file", siteModel1, user2);
-        //remove the user from site, but he keeps ownership on FILE_3_NAME 
+        createContent(FILE_2_NAME, "This is another Test file", siteModel1, user2);
+        // Remove user 2 from site, but he keeps ownership on FILE_2_NAME.
         dataUser.removeUserFromSite(user2, siteModel1);
+        // Also create another file that only user 2 has access to.
+        createContent(USER_2_FILE_NAME, "This is a test file that user1 does not have access to", siteModel2, user2);
     }
 
-    @TestRail (section = TestGroup.SEARCH,
-            executionType = ExecutionType.REGRESSION,
-            description = "Verify that we can perform a basic CMIS query against Elasticsearch.")
+    @TestRail (description = "Verify that we can perform a basic CMIS query against Elasticsearch.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
     @Test (groups = TestGroup.SEARCH)
     public void basicCMISQuery()
     {
@@ -110,9 +103,7 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
         searchQueryService.expectSomeResultsFromQuery(query, user1);
     }
 
-    @TestRail(section = TestGroup.SEARCH,
-              executionType = ExecutionType.REGRESSION,
-              description = "Verify that we can perform a basic CMIS query against the DB.")
+    @TestRail(description = "Verify that we can perform a basic CMIS query against the DB.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
     @Test(groups = TestGroup.SEARCH)
     public void basicCMISQueryAgainstDB()
     {
@@ -121,36 +112,27 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
         searchQueryService.expectSomeResultsFromQuery(query, user1);
     }
 
+    @TestRail (description = "Check we can use the CMIS LIKE syntax to match a prefix.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
+    @Test (groups = TestGroup.SEARCH)
+    public void matchNamesLikePrefix()
+    {
+        SearchRequest query = req("cmis", "SELECT * FROM cmis:document WHERE cmis:name LIKE '" + PREFIX + "%' AND CONTAINS('*')");
+        searchQueryService.expectResultsFromQuery(query, user1, FILE_0_NAME, FILE_1_NAME, FILE_2_NAME);
+    }
+
+    @TestRail (description = "Check we can use the CMIS CONTAINS syntax.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
+    @Test (groups = TestGroup.SEARCH)
+    public void matchContentOfFile()
+    {
+        // Check the query is case insensitive.
+        SearchRequest query = req("cmis", "SELECT * FROM cmis:document WHERE CONTAINS('test')");
+        searchQueryService.expectResultsFromQuery(query, user1, FILE_0_NAME, FILE_1_NAME, FILE_2_NAME);
+    }
+
     private FileModel createContent(String filename, String content, SiteModel site, UserModel user)
     {
         FileModel fileModel = new FileModel(filename, FileType.TEXT_PLAIN, content);
         return dataContent.usingUser(user).usingSite(site)
                           .createContent(fileModel);
-    }
-
-    private void createNodeWithProperties(SiteModel parentSite, FileModel fileModel, UserModel currentUser, Map<String, Object> properties)
-    {
-        AlfrescoHttpClient client = alfrescoHttpClientFactory.getObject();
-        String reqUrl = client.getApiVersionUrl() + "nodes/" + parentSite.getGuid() + "/children";
-        String name = fileModel.getName();
-
-        HttpPost post = new HttpPost(reqUrl);
-        JSONObject body = new JSONObject();
-        body.put("name", name);
-        body.put("nodeType", "cm:content");
-
-        JSONObject jsonProperties = new JSONObject();
-        jsonProperties.putAll(properties);
-        body.put("properties", jsonProperties);
-
-        post.setEntity(client.setMessageBody(body));
-
-        // Send Request
-        logger.info(String.format("POST: '%s'", reqUrl));
-        HttpResponse response = client.execute(currentUser.getUsername(), currentUser.getPassword(), post);
-        if (org.apache.http.HttpStatus.SC_CREATED != response.getStatusLine().getStatusCode())
-        {
-            throw new RuntimeException("Could not create file. Request response: " + client.getParameterFromJSON(response, "briefSummary", "error"));
-        }
     }
 }
