082b8a2d9b1e65aa506aba6fa006251d47c3e8ce
Tom Page <thomas.page@alfresco.com>
2022-04-04 16:31:03 +0100

    ACS-2752 Try moving reindexing tests to always run at the end of the suite.

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/reindexing/ElasticsearchReindexingTests.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/reindexing/ElasticsearchReindexingTests.java
@@ -1,4 +1,4 @@
-package org.alfresco.elasticsearch;
+package org.alfresco.elasticsearch.reindexing;
 
 import static org.alfresco.elasticsearch.AlfrescoStackInitializer.getElasticsearchConnectorImageTag;
 import static org.alfresco.elasticsearch.SearchQueryService.req;
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.alfresco.elasticsearch.AlfrescoStackInitializer;
+import org.alfresco.elasticsearch.SearchQueryService;
 import org.alfresco.rest.search.SearchRequest;
 import org.alfresco.utility.data.DataContent;
 import org.alfresco.utility.data.DataSite;
