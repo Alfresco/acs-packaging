a898dc4938813521b93d27aaa7d4c5ea9cd94390
KikoUK <kristian.dimitrov@alfresco.com>
2022-04-12 15:08:34 +0100

    ACS-2798: Add tests

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchCMISTests.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchCMISTests.java
@@ -104,6 +104,22 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
         searchQueryService.expectSomeResultsFromQuery(query, user1);
     }
 
+    @TestRail (description = "Verify that we can return a list of fields from a document via a CMIS query against Elasticsearch.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
+    @Test (groups = TestGroup.SEARCH)
+    public void returnFieldListFromDoc()
+    {
+        SearchRequest query = req("cmis", "SELECT cmis:name, cmis:objectId, cmis:lastModifiedBy, cmis:creationDate, cmis:contentStreamFileName FROM cmis:document");
+        searchQueryService.expectSomeResultsFromQuery(query, user1);
+    }
+
+    @TestRail (description = "Verify that we can return a list of fields from a folder via a CMIS query against Elasticsearch.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
+    @Test (groups = TestGroup.SEARCH)
+    public void returnFieldListFromFolder()
+    {
+        SearchRequest query = req("cmis", "SELECT cmis:name, cmis:parentId, cmis:path, cmis:allowedChildObjectTypeIds FROM cmis:folder");
+        searchQueryService.expectSomeResultsFromQuery(query, user1);
+    }
+
     @TestRail(description = "Verify that we can perform a basic CMIS query against the DB.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
     @Test(groups = TestGroup.SEARCH)
     public void basicCMISQueryAgainstDB()
