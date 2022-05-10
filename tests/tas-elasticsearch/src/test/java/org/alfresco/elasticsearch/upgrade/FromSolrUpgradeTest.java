2390af1aa2d67e8840d34038772bda7904e02760
pzurek <Piotr.Zurek@hyland.com>
2022-05-10 09:26:38 +0200

    ACS-2974 Search Service upgrade test

---
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/FromSolrUpgradeTest.java
@@ -0,0 +1,14 @@
+package org.alfresco.elasticsearch.upgrade;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class FromSolrUpgradeTest
+{
+    @Test
+    public void testIt()
+    {
+        assertTrue(false, "Expected failure");
+    }
+}
