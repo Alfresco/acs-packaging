c7a8a759a323e8fa3a361ffa5de396d3804f73bb
Aleksandra Onych <aleksandrao995@gmail.com>
2022-05-25 08:36:36 +0200

    ACS-2807 Elasticsearch CMIS E2E tests for cmis:baseTypeId field (#2233)
    * [ACS-2807] Elasticsearch CMIS E2E test for support cmis:baseTypeId field
    
    * [ACS-2807] Add a negative test for baseTypeIdQuery
    
    * [ACS-2807] refactor method name

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchCMISTests.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/ElasticsearchCMISTests.java
@@ -134,6 +134,14 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
         searchQueryService.expectResultsFromQuery(query2, user1, siteModel1.getId());
     }
 
+    @TestRail (description = "Check folders can be selected using cmis:baseTypeId.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
+    @Test (groups = TestGroup.SEARCH)
+    public void baseTypeIdQuery()
+    {
+        SearchRequest query = req("cmis", "SELECT * FROM cmis:folder WHERE cmis:baseTypeId = 'cmis:folder'");
+        searchQueryService.expectResultsFromQuery(query, user1, "documentLibrary", FOLDER_0_NAME, FOLDER_1_NAME, user1.getUsername(), siteModel1.getId());
+    }
+
     @TestRail (description = "Check we can use the CMIS LIKE syntax to match a prefix.", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
     @Test (groups = TestGroup.SEARCH)
     public void matchNamesLikePrefix()
@@ -225,6 +233,14 @@ public class ElasticsearchCMISTests extends AbstractTestNGSpringContextTests
 
         SearchRequest query3 = req("SELECT my:custom FROM cmis:document");
         searchQueryService.expectErrorFromQuery(query3, user1, HttpStatus.INTERNAL_SERVER_ERROR, "Unknown property: {http://www.alfresco.org/model/content/1.0}my");
+    }
+
+    @Test (groups = TestGroup.SEARCH)
+    public void negative_baseTypeIdQuery_invalidType()
+    {
+        // note: ideally 400 but currently 500 (also for Solr) :-(
+        SearchRequest query = req("SELECT * FROM cmis:folder WHERE cmis:baseTypeId = 'cmis:unknown'");
+        searchQueryService.expectErrorFromQuery(query, user1, HttpStatus.INTERNAL_SERVER_ERROR, "Unknown property: {http://www.alfresco.org/model/content/1.0}cmis");
     }
 
     @TestRail (description = "Check all folders can be selected (including sites/doc libs).", section = TestGroup.SEARCH, executionType = ExecutionType.REGRESSION)
