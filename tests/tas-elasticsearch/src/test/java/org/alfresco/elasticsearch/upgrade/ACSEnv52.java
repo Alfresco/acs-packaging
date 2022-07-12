cd1d531b9f44a2c53b9215d337a6a42ade0aab05
pzurek <Piotr.Zurek@hyland.com>
2022-07-12 18:43:02 +0200

    ACS-3141 ACS-3140 Upgrade scenario from ACS5.2 with Solr to ACS7.3 with Elasticsearch

---
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/ACSEnv52.java
@@ -0,0 +1,84 @@
+package org.alfresco.elasticsearch.upgrade;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class ACSEnv52 extends BaseACSEnv
+{
+    private final GenericContainer<?> postgres;
+    private final GenericContainer<?> alfresco;
+
+    public ACSEnv52(final Network network)
+    {
+        postgres = createPostgresContainer(network);
+
+        createLibreOfficeContainer(network);
+        createSolr6Container(network);
+
+        alfresco = createRepositoryContainer(network);
+    }
+
+    @Override
+    protected GenericContainer<?> getAlfresco()
+    {
+        return alfresco;
+    }
+
+    @Override
+    protected GenericContainer<?> getPostgres()
+    {
+        return postgres;
+    }
+
+    @Override
+    protected String getContainerAlfDataPath()
+    {
+        return "/usr/local/alf_data";
+    }
+
+    private GenericContainer<?> createPostgresContainer(Network network)
+    {
+        return newContainer(PostgreSQLContainer.class, "postgres:9.4")
+                .withPassword("alfresco")
+                .withUsername("alfresco")
+                .withDatabaseName("alfresco")
+                .withNetwork(network)
+                .withNetworkAliases("postgres");
+    }
+
+    private void createLibreOfficeContainer(Network network)
+    {
+        newContainer(GenericContainer.class, "xcgd/libreoffice")
+                .withNetwork(network)
+                .withNetworkAliases("libreoffice");
+    }
+
+    private void createSolr6Container(Network network)
+    {
+        newContainer(GenericContainer.class,"alfresco/alfresco-search-services:1.3.0.6")
+                .withEnv("SOLR_ALFRESCO_HOST", "alfresco")
+                .withEnv("SOLR_ALFRESCO_PORT", "8080")
+                .withEnv("ALFRESCO_SECURE_COMMS", "none")
+                .withEnv("SOLR_SOLR_HOST", "solr6")
+                .withEnv("SOLR_SOLR_PORT", "8983")
+                .withEnv("SOLR_CREATE_ALFRESCO_DEFAULTS", "alfresco,archive")
+                .withEnv("SOLR_JAVA_MEM", "-Xms1g -Xmx1g")
+                .withNetwork(network)
+                .withNetworkAliases("solr6");
+    }
+
+    private GenericContainer<?> createRepositoryContainer(Network network)
+    {
+        return newContainer(GenericContainer.class, "quay.io/alfresco/alfresco-content-repository-52:5.2.6")
+                .withExposedPorts(8080)
+                .withEnv("JAVA_TOOL_OPTIONS",
+                        "-Dsolr.host=solr6 " +
+                                "-Dsolr.port=8983 " +
+                                "-Dsolr.secureComms=none "+
+                                "-Dsolr.base.url=/solr " +
+                                "-Dindex.subsystem.name=solr6")
+                .withNetwork(network)
+                .withNetworkAliases("alfresco");
+    }
+}
--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/FromOldACSWithSolrUpgradeTest.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/FromOldACSWithSolrUpgradeTest.java
@@ -3,28 +3,17 @@ package org.alfresco.elasticsearch.upgrade;
 import static java.time.Duration.ofMinutes;
 
 import static org.alfresco.elasticsearch.AlfrescoStackInitializer.getImagesConfig;
-import static org.alfresco.elasticsearch.upgrade.Utils.waitFor;
+import static org.alfresco.elasticsearch.upgrade.Utils.createNetwork;
+import static org.alfresco.elasticsearch.upgrade.Utils.createTempContentStoreDirectory;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URL;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.time.Duration;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.alfresco.elasticsearch.EnvHelper;
 import org.junit.Test;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.Container.ExecResult;
-import org.testcontainers.containers.GenericContainer;
+import org.springframework.util.FileSystemUtils;
 import org.testcontainers.containers.Network;
 import org.testng.Assert;
 
@@ -32,179 +21,68 @@ public class FromOldACSWithSolrUpgradeTest
 {
     private static final URL TEST_FILE_URL = FromSolrUpgradeTest.class.getResource("babekyrtso.pdf");
     private static final String SEARCH_TERM = "babekyrtso";
-    public static final String AFTER_STARTUP_PDF = "after-startup.pdf";
+    public static final String FILE_UPLOADED_AFTER_INITIAL_ENVIRONMENT_STARTUP = "after-startup.pdf";
+    public static final String FILE_UPLOADED_AFTER_TAKING_DUMP = "after-dump.pdf";
+    public static final String FILE_UPLOADED_BEFORE_UPGRADING_INITIAL_ENVIRONMENT = "before-upgrade.pdf";
 
     @Test
-    public void testIt() throws InterruptedException, IOException
+    public void testIt() throws IOException
     {
         final Path oldEnvContentStorePath = createTempContentStoreDirectory();
         final Path mirroredEnvContentStorePath = createTempContentStoreDirectory();
 
-        Network initialEnvNetwork = Network
-                .builder()
-                .createNetworkCmdModifier(cmd -> cmd.withAttachable(true).withName("B" + UUID.randomUUID()))
-                .build();
-        final Network mirroredEnvNetwork = Network
-                .builder()
-                .createNetworkCmdModifier(cmd -> cmd.withAttachable(true).withName("A" + UUID.randomUUID()))
-                .build();
+        final Network initialEnvNetwork = createNetwork("B");
+        final Network mirroredEnvNetwork = createNetwork("A");
 
-        GenericContainer postgres = new GenericContainer<>("postgres:9.4")
-                .withEnv("POSTGRES_PASSWORD", "alfresco")
-                .withEnv("POSTGRES_USER", "alfresco")
-                .withEnv("POSTGRES_DB", "alfresco")
-                .withCommand("postgres -c max_connections=300 -c log_min_messages=LOG")
-                .withNetwork(initialEnvNetwork)
-                .withNetworkAliases("postgres");
-        postgres.start();
-
-        GenericContainer libreoffice = new GenericContainer("xcgd/libreoffice")
-                .withNetwork(initialEnvNetwork)
-                .withNetworkAliases("libreoffice");
-        libreoffice.start();
-
-        GenericContainer alfresco = new GenericContainer<>("quay.io/alfresco/alfresco-content-repository-52:5.2.6")
-                .withExposedPorts(8080)
-                .withEnv("JAVA_TOOL_OPTIONS", "-Dsolr.host=solr6 -Dsolr.port=8983 -Dsolr.secureComms=none -Dsolr.base.url=/solr -Dindex.subsystem.name=solr6")
-                .withNetwork(initialEnvNetwork)
-                .withNetworkAliases("alfresco")
-                .withLogConsumer(of -> System.err.print("[alfresco] " + of.getUtf8String()))
-                .withFileSystemBind(oldEnvContentStorePath.toAbsolutePath().toString(), "/usr/local/alf_data", BindMode.READ_WRITE);
-        alfresco.start();
-
-        GenericContainer solr6 = new GenericContainer<>("alfresco/alfresco-search-services:1.3.0.6")
-                .withEnv("SOLR_ALFRESCO_HOST", "alfresco")
-                .withEnv("SOLR_ALFRESCO_PORT", "8080")
-                .withEnv("ALFRESCO_SECURE_COMMS", "none")
-                .withEnv("SOLR_SOLR_HOST", "solr6")
-                .withEnv("SOLR_SOLR_PORT", "8983")
-                .withEnv("SOLR_CREATE_ALFRESCO_DEFAULTS", "alfresco,archive")
-                .withEnv("SOLR_JAVA_MEM", "-Xms1g -Xmx1g")
-                .withNetwork(initialEnvNetwork)
-                .withNetworkAliases("solr6")
-                .withLogConsumer(of -> System.err.print("[solr6] " + of.getUtf8String()));
-        solr6.start();
-
-        GenericContainer share = new GenericContainer("quay.io/alfresco/alfresco-share-52:5.2.6")
-                .withEnv("MEM_LIMIT", "1200m")
-                .withNetwork(initialEnvNetwork)
-                .withNetworkAliases("share")
-                .withExposedPorts(8080);
-        share.start();
-
-        RepoHttpClient repoHttpClient = new RepoHttpClient(URI.create("http://" + alfresco.getHost() + ":" + alfresco.getMappedPort(8080)));
-        expectNoSearchResult(repoHttpClient, ofMinutes(5), UUID.randomUUID().toString());
-
-        uploadFile(repoHttpClient, TEST_FILE_URL, AFTER_STARTUP_PDF);
-        expectSearchResult(repoHttpClient, ofMinutes(1), SEARCH_TERM, AFTER_STARTUP_PDF);
-
-        String dump = getMetadataDump(postgres);
-        System.out.println(dump);
-
-        Elasticsearch elasticsearch = new Elasticsearch(getUpgradeScenarioConfig(), mirroredEnvNetwork, initialEnvNetwork);
-
-        ACSEnv mirroredEnv = new ACSEnv(getUpgradeScenarioConfig(), mirroredEnvNetwork, "elasticsearch");
-        mirroredEnv.setReadOnlyContentStoreHostPath(mirroredEnvContentStorePath);
-        mirroredEnv.setMetadataDumpToRestore(dump);
-
-
-        elasticsearch.start();
-        Assert.assertFalse(elasticsearch.isIndexCreated());
-
-        mirroredEnv.start();
-
-        //TODO: adjust path to license
-        Assert.assertTrue(mirroredEnv.uploadLicense("src/test/resources/alf73-allenabled.lic"));
-
-        mirroredEnv.expectNoSearchResult(ofMinutes(1), SEARCH_TERM);
-
-        Assert.assertTrue(elasticsearch.isIndexCreated());
-        Assert.assertEquals(elasticsearch.getIndexedDocumentCount(), 0);
-
-        mirroredEnv.expectNoSearchResult(ofMinutes(1), SEARCH_TERM);
-        mirroredEnv.startLiveIndexing();
-
-        final long initialReIndexingUpperBound = mirroredEnv.getMaxNodeDbId();
-        mirroredEnv.reindexByIds(0, initialReIndexingUpperBound);
-
-        Assert.assertTrue(elasticsearch.getIndexedDocumentCount() > 0);
-        mirroredEnv.expectSearchResult(ofMinutes(1), SEARCH_TERM, AFTER_STARTUP_PDF);
-
-
-
-        Thread.sleep(10000);
-    }
-
-    private static Path createTempContentStoreDirectory()
-    {
-        try
+        try (final ACSEnv52 acs52 = new ACSEnv52(initialEnvNetwork))
         {
-            final Path tempDir = Files.createTempDirectory("alf_data");
-            if (FileSystems.getDefault().supportedFileAttributeViews().contains("posix"))
+            acs52.setContentStoreHostPath(oldEnvContentStorePath);
+            acs52.start();
+
+            acs52.expectNoSearchResult(ofMinutes(5), UUID.randomUUID().toString());
+            acs52.uploadFile(TEST_FILE_URL, FILE_UPLOADED_AFTER_INITIAL_ENVIRONMENT_STARTUP);
+            acs52.expectSearchResult(ofMinutes(1), SEARCH_TERM, FILE_UPLOADED_AFTER_INITIAL_ENVIRONMENT_STARTUP);
+
+            String dump = acs52.getMetadataDump();
+
+            acs52.uploadFile(TEST_FILE_URL, FILE_UPLOADED_AFTER_TAKING_DUMP);
+            acs52.expectSearchResult(ofMinutes(1), SEARCH_TERM, FILE_UPLOADED_AFTER_INITIAL_ENVIRONMENT_STARTUP, FILE_UPLOADED_AFTER_TAKING_DUMP);
+
+            final Elasticsearch elasticsearch = new Elasticsearch(getUpgradeScenarioConfig(), mirroredEnvNetwork, initialEnvNetwork);
+
+            try (final ACSEnv mirroredEnv = new ACSEnv(getUpgradeScenarioConfig(), mirroredEnvNetwork, "elasticsearch"))
             {
-                Files.setPosixFilePermissions(tempDir, PosixFilePermissions.fromString("rwxrwxrwx"));
+                mirroredEnv.setContentStoreHostPath(mirroredEnvContentStorePath);
+                mirroredEnv.setMetadataDumpToRestore(dump);
+                FileSystemUtils.copyRecursively(oldEnvContentStorePath, mirroredEnvContentStorePath);
+
+                elasticsearch.start();
+                Assert.assertFalse(elasticsearch.isIndexCreated());
+
+                mirroredEnv.start();
+
+                Assert.assertTrue(mirroredEnv.uploadLicense("/Users/pzurek/Downloads/alf73-allenabled.lic"));
+
+                mirroredEnv.expectNoSearchResult(ofMinutes(1), SEARCH_TERM);
+
+                Assert.assertTrue(elasticsearch.isIndexCreated());
+                Assert.assertEquals(elasticsearch.getIndexedDocumentCount(), 0);
+
+                mirroredEnv.startLiveIndexing();
+
+                final long initialReIndexingUpperBound = mirroredEnv.getMaxNodeDbId();
+                mirroredEnv.reindexByIds(0, initialReIndexingUpperBound);
+
+                Assert.assertTrue(elasticsearch.getIndexedDocumentCount() > 0);
+                mirroredEnv.expectSearchResult(ofMinutes(1), SEARCH_TERM, FILE_UPLOADED_AFTER_INITIAL_ENVIRONMENT_STARTUP);
             }
-            return tempDir;
-        } catch (IOException e)
-        {
-            throw new RuntimeException("Couldn't create aa temp directory.", e);
+
+            acs52.uploadFile(TEST_FILE_URL, FILE_UPLOADED_BEFORE_UPGRADING_INITIAL_ENVIRONMENT);
+            acs52.expectSearchResult(ofMinutes(1), SEARCH_TERM,
+                    FILE_UPLOADED_AFTER_INITIAL_ENVIRONMENT_STARTUP,
+                    FILE_UPLOADED_AFTER_TAKING_DUMP,
+                    FILE_UPLOADED_BEFORE_UPGRADING_INITIAL_ENVIRONMENT);
         }
-    }
-
-    public String getMetadataDump(GenericContainer postgres)
-    {
-        return execInPostgres(postgres, "pg_dump -c -U alfresco alfresco").getStdout();
-    }
-
-    private ExecResult execInPostgres(GenericContainer postgres, String command)
-    {
-        final ExecResult result;
-        try
-        {
-            result = postgres.execInContainer("sh", "-c", command);
-        } catch (IOException e)
-        {
-            throw new RuntimeException("Failed to execute command `" + command + "`.", e);
-        } catch (InterruptedException e)
-        {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("Command execution has been interrupted..", e);
-        }
-        if (result.getExitCode() != 0)
-        {
-            throw new RuntimeException("Failed to execute command `" + command + "`. " + result.getStderr());
-        }
-
-        return result;
-    }
-
-    public UUID uploadFile(RepoHttpClient repoHttpClient, URL contentUrl, String fileName) throws IOException
-    {
-        return repoHttpClient.uploadFile(contentUrl, fileName);
-    }
-
-    public void expectNoSearchResult(RepoHttpClient repoHttpClient, Duration timeout, String term)
-    {
-        expectSearchResult(repoHttpClient, timeout, term);
-    }
-
-    public void expectSearchResult(RepoHttpClient repoHttpClient, Duration timeout, String term, String... expectedFiles)
-    {
-        final Set<String> expected = Stream
-                .of(expectedFiles)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toUnmodifiableSet());
-
-        waitFor("Reaching the point where `" + expected + "` is returned.", timeout, () -> {
-            try
-            {
-                Optional<Set<String>> actual = repoHttpClient.searchForFiles(term);
-                return actual.map(expected::equals).orElse(false);
-            } catch (IOException e)
-            {
-                return false;
-            }
-        });
     }
 
     private Config getUpgradeScenarioConfig()
