6e6455853dbb948ab5d881932e0d95bd5d4c7aef
pzurek <Piotr.Zurek@hyland.com>
2022-07-18 14:43:47 +0200

    ACS-3141 ACS-3140 Upgrade scenario from ACS5.2 with Solr to ACS7.3 with Elasticsearch

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/LegacyAcsUpgradeScenario.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/LegacyAcsUpgradeScenario.java
@@ -4,7 +4,12 @@ import static org.alfresco.elasticsearch.upgrade.Utils.createNetwork;
 import static org.alfresco.elasticsearch.upgrade.Utils.createTempContentStoreDirectory;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.PosixFilePermissions;
 
 import org.springframework.util.FileSystemUtils;
 import org.testcontainers.containers.Network;
@@ -51,6 +56,25 @@ public class LegacyAcsUpgradeScenario implements AutoCloseable
         mirroredEnv.setMetadataDumpToRestore(initialEnv.getMetadataDump());
         try
         {
+            System.out.println("initialEnvContentStorePath: " + initialEnvContentStorePath);
+            System.out.println("mirroredEnvContentStorePath: " + mirroredEnvContentStorePath);
+            Files.walkFileTree(initialEnvContentStorePath, new SimpleFileVisitor<Path>(){
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+                {
+                    System.out.print(file);
+                    System.out.println(" F-> " + PosixFilePermissions.toString(Files.getPosixFilePermissions(file)));
+                    return super.visitFile(file, attrs);
+                }
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException
+                {
+                    System.out.print(dir);
+                    System.out.println(" F-> " + PosixFilePermissions.toString(Files.getPosixFilePermissions(file)));
+                    return super.preVisitDirectory(dir, attrs);
+                }
+            });
             FileSystemUtils.copyRecursively(initialEnvContentStorePath, mirroredEnvContentStorePath);
         }
         catch (IOException e)
