9a03c935bcc11f3a4d709907cb96d0380f8e9997
pzurek <Piotr.Zurek@hyland.com>
2022-07-26 12:28:41 +0200

    ACS-3141 ACS-3140 ACS-3324 Upgrade scenario from ACS5.2 with Solr to ACS7.3 with Elasticsearch

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/LegacyAcsUpgradeScenario.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/upgrade/LegacyAcsUpgradeScenario.java
@@ -48,8 +48,8 @@ public class LegacyAcsUpgradeScenario implements AutoCloseable
     {
         try
         {
+            initialEnv.exposeContentStore();
             IOUtils.copy(Runtime.getRuntime().exec("ls -alh " + sharedContentStorePath).getInputStream(), System.out);
-            mirroredEnv.exposeContentStore();
         } catch (IOException e)
         {
             throw new RuntimeException(e);
