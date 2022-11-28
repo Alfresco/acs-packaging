579f40433049dc77314d68ea4fb7e19787af867d
kcichonczyk <kamil.cichonczyk@hyland.com>
2022-11-28 15:06:44 +0100

    [ACS-4042] removed databaseName property from the url

--- tests/testcontainers-env/src/main/java/org/alfresco/tas/DatabaseType.java
+++ tests/testcontainers-env/src/main/java/org/alfresco/tas/DatabaseType.java
@@ -9,7 +9,7 @@ public enum DatabaseType
     POSTGRESQL_DB("postgresql", "org.postgresql.Driver", "jdbc:postgresql://postgres:5432/alfresco", "alfresco", "alfresco"),
     MYSQL_DB("mysql", "com.mysql.cj.jdbc.Driver", "jdbc:mysql://mysql:3306/alfresco", "alfresco", "alfresco"),
     MARIA_DB("mariadb", "org.mariadb.jdbc.Driver", "jdbc:mariadb://mariadb:3306/alfresco", "alfresco", "alfresco"),
-    MSSQL_DB("mssql", "com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:sqlserver://mssql:1433\\;user=sa\\;password=Alfresco1\\;databaseName=alfresco\\;lockTimeout=1000", "sa", "Alfresco1", Map.of("txn.isolation", "4096", "pool.max", "275"));
+    MSSQL_DB("mssql", "com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:sqlserver://mssql:1433\\;user=sa\\;password=Alfresco1\\;lockTimeout=1000", "sa", "Alfresco1", Map.of("txn.isolation", "4096", "pool.max", "275"));
 
     private final String type;
     private final String driver;
