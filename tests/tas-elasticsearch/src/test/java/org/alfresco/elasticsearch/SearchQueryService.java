673a241da8353739bbf48519ec8de22943606875
mstrankowski <marcin.strankowski@hyland.com>
2022-12-08 08:51:22 +0100

    Reduce fail retry timeout timer to 1 minute from 10 minutes, if 1 minute won't be enough, then it probably should fail anyways

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/SearchQueryService.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/SearchQueryService.java
@@ -92,7 +92,7 @@ public class SearchQueryService
     {
         try
         {
-            Utility.sleep(1000, 600000, () ->
+            Utility.sleep(1000, 60000, () ->
             {
                 SearchResponse response = client.authenticateUser(user)
                                                 .withSearchAPI()
