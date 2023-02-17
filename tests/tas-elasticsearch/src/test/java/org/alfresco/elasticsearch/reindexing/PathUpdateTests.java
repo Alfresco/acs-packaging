2aab6f8302f1d3db0c46ba0210e2f27ad90bcd96
Tom Page <thomas.page@alfresco.com>
2023-02-17 17:05:57 +0000

    ACS-4637 Add one second pause between given and when steps.

--- tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/reindexing/PathUpdateTests.java
+++ tests/tas-elasticsearch/src/test/java/org/alfresco/elasticsearch/reindexing/PathUpdateTests.java
@@ -18,6 +18,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -48,16 +49,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testcontainers.shaded.org.apache.commons.lang3.tuple.Pair;
+import org.testcontainers.shaded.org.apache.commons.lang3.tuple.Triple;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
  * Tests to check that paths are updated correctly.
  * <p>
- * These tests use an unusual structure where the bulk of the code is stored in lambdas within the {@#testWhenAndThenSteps} method.
- * All the "when" steps are run in the `@BeforeClass` method and then the "then" steps are run in the individual `@Test` methods.
- * This structure saves about 1 second per test over having the "when" steps in the `@Test` methods.
+ * These tests use an unusual structure where the bulk of the code is stored in lambdas within the {@#testSteps} method.
+ * All the "given" and "when" steps are run in the `@BeforeClass` method (with a one second pause between them) and then
+ * the "then" steps are run in the individual `@Test` methods.
+ * This structure saves about two seconds per test over having the "given" and "when" steps in the `@Test` methods.
  */
 @ContextConfiguration (locations = "classpath:alfresco-elasticsearch-context.xml",
         initializers = AlfrescoStackInitializer.class)
@@ -84,12 +86,16 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
     /** Map from test method name to the set of named properties used by the test. */
     private Map<String, Map<String, Object>> propertyMaps;
 
-    /** All the "when" and "then" steps for each test, grouped according to the test name. */
-    private Map<String, Pair<Supplier<Map<String, Object>>, Consumer<Map<String, Object>>>> testWhenAndThenSteps = Map.of(
-            "testChangeFileNameUpdatesPath", Pair.of(() -> {
+    /** All the "given", "when" and "then" steps for each test, grouped according to the test name. */
+    private Map<String, Triple<Supplier<Map<String, Object>>, Function<Map<String, Object>, Map<String, Object>>, Consumer<Map<String, Object>>>> testSteps = Map.of(
+            "testChangeFileNameUpdatesPath", Triple.of(() -> {
                 Step.STEP("Create a file in the site.");
                 FileModel fileModel = FileModel.getRandomFileModel(TEXT_PLAIN);
                 FileModel testFile = dataContent.usingUser(testUser).usingSite(testSite).createContent(fileModel);
+
+                return Map.of("testFile", testFile);
+            }, (properties) -> {
+                FileModel testFile = (FileModel) properties.get("testFile");
 
                 Step.STEP("Update the filename.");
                 RestNodeModel updatedFile = renameNode(restClient, testUser, testFile);
@@ -103,11 +109,16 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 searchQueryService.expectResultsFromQuery(query, testUser, updatedName);
             }),
 
-            "testChangeFileParentUpdatesPath", Pair.of(() -> {
+            "testChangeFileParentUpdatesPath", Triple.of(() -> {
                 Step.STEP("Create a file next to a folder.");
                 FileModel fileModel = FileModel.getRandomFileModel(TEXT_PLAIN);
                 FileModel testFile = dataContent.usingUser(testUser).usingSite(testSite).createContent(fileModel);
                 FolderModel testFolder = dataContent.usingUser(testUser).usingSite(testSite).createFolder();
+
+                return Map.of("testFile", testFile, "testFolder", testFolder);
+            }, (properties) -> {
+                FileModel testFile = (FileModel) properties.get("testFile");
+                FolderModel testFolder = (FolderModel) properties.get("testFolder");
 
                 Step.STEP("Move the file into the folder and check the path is updated.");
                 moveNode(restClient, testUser, testFile, testFolder);
@@ -122,14 +133,16 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 searchQueryService.expectResultsFromQuery(query, testUser, fileName);
             }),
 
-            "testChangeFolderNameUpdatesPath", Pair.of(() -> {
+            "testChangeFolderNameUpdatesPath", Triple.of(() -> {
                 Step.STEP("Create a file in a folder.");
                 FolderModel testFolder = dataContent.usingUser(testUser).usingSite(testSite).createFolder();
                 FileModel fileModel = FileModel.getRandomFileModel(TEXT_PLAIN);
                 FileModel testFile = dataContent.usingUser(testUser).usingResource(testFolder).createContent(fileModel);
 
-                // Wait for a second to ensure the folder update event timestamp is different from the file create timestamp.
-                waitForOneSecond();
+                return Map.of("testFile", testFile, "testFolder", testFolder);
+            }, (properties) -> {
+                FileModel testFile = (FileModel) properties.get("testFile");
+                FolderModel testFolder = (FolderModel) properties.get("testFolder");
 
                 Step.STEP("Update the folder's name.");
                 RestNodeModel updatedFolder = renameNode(restClient, testUser, testFolder);
@@ -144,12 +157,18 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 searchQueryService.expectResultsFromQuery(query, testUser, fileName);
             }),
 
-            "testChangeFolderParentUpdatesPath", Pair.of(() -> {
+            "testChangeFolderParentUpdatesPath", Triple.of(() -> {
                 Step.STEP("Create two folders with a file in the first.");
                 FolderModel firstFolder = dataContent.usingUser(testUser).usingSite(testSite).createFolder();
                 FolderModel secondFolder = dataContent.usingUser(testUser).usingSite(testSite).createFolder();
                 FileModel fileModel = FileModel.getRandomFileModel(TEXT_PLAIN);
                 FileModel testFile = dataContent.usingUser(testUser).usingResource(firstFolder).createContent(fileModel);
+
+                return Map.of("testFile", testFile, "firstFolder", firstFolder, "secondFolder", secondFolder);
+            }, (properties) -> {
+                FileModel testFile = (FileModel) properties.get("testFile");
+                FolderModel firstFolder = (FolderModel) properties.get("firstFolder");
+                FolderModel secondFolder = (FolderModel) properties.get("secondFolder");
 
                 Step.STEP("Move the first folder into the second.");
                 moveNode(restClient, testUser, firstFolder, secondFolder);
@@ -165,7 +184,7 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 searchQueryService.expectResultsFromQuery(query, testUser, fileName);
             }),
 
-            "testChangeCategoriesUpdatesPath", Pair.of(() -> {
+            "testChangeCategoriesUpdatesPath", Triple.of(() -> {
                 Step.STEP("Create two categories and a file in the first.");
                 RestCategoryModel firstCategory = createCategory(restClient, dataUser.getAdminUser(), "-root-");
                 RestCategoryModel secondCategory = createCategory(restClient, dataUser.getAdminUser(), "-root-");
@@ -173,6 +192,12 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 FileModel testFile = dataContent.usingUser(testUser).usingSite(testSite).createContent(fileModel);
                 restClient.authenticateUser(testUser).withCoreAPI().usingNode(testFile)
                           .linkToCategory(RestCategoryLinkBodyModel.builder().categoryId(firstCategory.getId()).create());
+
+                return Map.of("testFile", testFile, "firstCategory", firstCategory, "secondCategory", secondCategory);
+            }, (properties) -> {
+                FileModel testFile = (FileModel) properties.get("testFile");
+                RestCategoryModel firstCategory = (RestCategoryModel) properties.get("firstCategory");
+                RestCategoryModel secondCategory = (RestCategoryModel) properties.get("secondCategory");
 
                 Step.STEP("Remove the file from the first category and add it to the second.");
                 restClient.authenticateUser(testUser).withCoreAPI().usingNode(testFile)
@@ -195,7 +220,7 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 searchQueryService.expectResultsFromQuery(query, testUser, fileName);
             }),
 
-            "testChangeCategoryNameUpdatesPath", Pair.of(() -> {
+            "testChangeCategoryNameUpdatesPath", Triple.of(() -> {
                 Step.STEP("Create a category and a file in it.");
                 RestCategoryModel category = createCategory(restClient, dataUser.getAdminUser(), "-root-");
                 FileModel fileModel = FileModel.getRandomFileModel(TEXT_PLAIN);
@@ -203,8 +228,10 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 restClient.authenticateUser(testUser).withCoreAPI().usingNode(testFile)
                           .linkToCategory(RestCategoryLinkBodyModel.builder().categoryId(category.getId()).create());
 
-                // Wait for a second to ensure the category update event timestamp is different from the file create timestamp.
-                waitForOneSecond();
+                return Map.of("testFile", testFile, "category", category);
+            }, (properties) -> {
+                FileModel testFile = (FileModel) properties.get("testFile");
+                RestCategoryModel category = (RestCategoryModel) properties.get("category");
 
                 Step.STEP("Update the name of the category.");
                 String newCategoryName = category.getName() + "_updated";
@@ -222,7 +249,7 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 searchQueryService.expectResultsFromQuery(query, testUser, fileName);
             }),
 
-            "testChangeCategoryPathUpdatesPath", Pair.of(() -> {
+            "testChangeCategoryPathUpdatesPath", Triple.of(() -> {
                 Step.STEP("Create two nested categories and assign a file to the child.");
                 RestCategoryModel parentCategory = createCategory(restClient, dataUser.getAdminUser(), "-root-");
                 RestCategoryModel childCategory = createCategory(restClient, dataUser.getAdminUser(), parentCategory.getId());
@@ -231,8 +258,11 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 restClient.authenticateUser(testUser).withCoreAPI().usingNode(testFile)
                           .linkToCategory(RestCategoryLinkBodyModel.builder().categoryId(childCategory.getId()).create());
 
-                // Wait for a second to ensure the category update event timestamp is different from the file create timestamp.
-                //waitForOneSecond();
+                return Map.of("testFile", testFile, "childCategory", childCategory, "parentCategory", parentCategory);
+            }, (properties) -> {
+                FileModel testFile = (FileModel) properties.get("testFile");
+                RestCategoryModel childCategory = (RestCategoryModel) properties.get("childCategory");
+                RestCategoryModel parentCategory = (RestCategoryModel) properties.get("parentCategory");
 
                 Step.STEP("Update the parent category name and check the file's paths are updated.");
                 String newCategoryName = parentCategory.getName() + "_updated";
@@ -240,7 +270,7 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                           .usingCategory(parentCategory)
                           .updateCategory(RestCategoryModel.builder().name(newCategoryName).create());
 
-                return Map.of("fileName", testFile.getName(), "childCategoryName", childCategory.getName(),"parentCategoryName", parentCategory.getName());
+                return Map.of("fileName", testFile.getName(), "childCategoryName", childCategory.getName(),"parentCategoryName", newCategoryName);
             }, (properties) -> {
                 String fileName = (String) properties.get("fileName");
                 String childCategoryName = (String) properties.get("childCategoryName");
@@ -250,7 +280,7 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 SearchRequest query = req("PATH:\"" + categoryPath(parentCategoryName, childCategoryName, fileName) + "\"");
                 searchQueryService.expectResultsFromQuery(query, testUser, fileName);
             })
-        );
+    );
 
     /**
      * Create a user and a private site containing some nested folders with a document in.
@@ -274,64 +304,74 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
                 .filter(method -> stream(method.getAnnotations()).anyMatch(Test.class::isInstance))
                 .map(Method::getName)
                 .collect(Collectors.toSet());
-        assertEquals("Some entries in testWhenAndThenSteps do not have corresponding @Test methods.", emptySet(), Sets.difference(testWhenAndThenSteps.keySet(), testMethods));
+        assertEquals("Some entries in testWhenAndThenSteps do not have corresponding @Test methods.", emptySet(), Sets.difference(testSteps.keySet(), testMethods));
+
+        // Execute the "given" step for all tests and store the state to be passed into the "when" steps.
+        propertyMaps = testSteps.entrySet()
+                                .stream()
+                                .collect(toMap(Entry::getKey,
+                                                    entry -> testSteps.get(entry.getKey())
+                                                                      .getLeft()
+                                                                      .get()));
+
+        waitForOneSecond();
 
         // Execute the "when" step for all tests and store the state to be passed into the "then" steps.
-        propertyMaps = testWhenAndThenSteps.entrySet()
-                                           .stream()
-                                           .collect(toMap(Entry::getKey,
-                                                    entry -> testWhenAndThenSteps.get(entry.getKey())
-                                                                                 .getLeft()
-                                                                                 .get()));
+        propertyMaps = testSteps.entrySet()
+                                .stream()
+                                .collect(toMap(Entry::getKey,
+                                        entry -> testSteps.get(entry.getKey())
+                                                          .getMiddle()
+                                                          .apply(propertyMaps.get(entry.getKey()))));
     }
 
     @Test
     public void testChangeFileNameUpdatesPath()
     {
         String methodName = StackWalker.getInstance().walk(Stream::findFirst).get().getMethodName();
-        testWhenAndThenSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
+        testSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
     }
 
     @Test
     public void testChangeFileParentUpdatesPath()
     {
         String methodName = StackWalker.getInstance().walk(Stream::findFirst).get().getMethodName();
-        testWhenAndThenSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
+        testSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
     }
 
     @Test
     public void testChangeFolderNameUpdatesPath()
     {
         String methodName = StackWalker.getInstance().walk(Stream::findFirst).get().getMethodName();
-        testWhenAndThenSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
+        testSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
     }
 
     @Test
     public void testChangeFolderParentUpdatesPath()
     {
         String methodName = StackWalker.getInstance().walk(Stream::findFirst).get().getMethodName();
-        testWhenAndThenSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
+        testSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
     }
 
     @Test
     public void testChangeCategoriesUpdatesPath()
     {
         String methodName = StackWalker.getInstance().walk(Stream::findFirst).get().getMethodName();
-        testWhenAndThenSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
+        testSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
     }
 
     @Test
     public void testChangeCategoryNameUpdatesPath()
     {
         String methodName = StackWalker.getInstance().walk(Stream::findFirst).get().getMethodName();
-        testWhenAndThenSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
+        testSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
     }
 
     @Test
     public void testChangeCategoryPathUpdatesPath()
     {
         String methodName = StackWalker.getInstance().walk(Stream::findFirst).get().getMethodName();
-        testWhenAndThenSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
+        testSteps.get(methodName).getRight().accept(propertyMaps.get(methodName));
     }
 
     /**
@@ -405,7 +445,9 @@ public class PathUpdateTests extends AbstractTestNGSpringContextTests
         return "/cm:categoryRoot/cm:generalclassifiable/cm:" + Arrays.stream(nodeNames).collect(joining("/cm:"));
     }
 
-    /** Wait for a second. */
+    /**
+     * Wait for a second. This is needed due to the index refresh interval - see ACS-4637.
+     */
     private static void waitForOneSecond()
     {
         try
