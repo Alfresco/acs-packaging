9d84d64adc17ee976aee3194037838d6e838bd19
pzurek <Piotr.Zurek@hyland.com>
2023-02-20 09:19:24 +0100

    Increase max_clause_count

--- tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
+++ tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
@@ -238,7 +238,7 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
                 .withExposedPorts(9200)
                 .withEnv("plugins.security.disabled", "true")
                 .withEnv("discovery.type", "single-node")
-                .withEnv("indices.query.bool.max_clause_count", "16384")
+                .withEnv("indices.query.bool.max_clause_count", "32768")
                 .withEnv("OPENSEARCH_JAVA_OPTS", "-Xms1g -Xmx1g")
                 .withCreateContainerCmdModifier(MEDIUM_RAM_LIMIT);
     }
