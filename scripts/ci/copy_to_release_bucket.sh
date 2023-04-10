f2620f56568b3e10c970171496f2dcc80d6a1b36
evasques <eva.vasques@gmail.com>
2023-04-10 11:49:54 +0100

    Migrate to GHA (#2435)
    * Remove Travis configuration
    * Add GHA configuration
    * Add AlphabeticalPriorityInterceptor for elasticsearch tests

--- scripts/ci/copy_to_release_bucket.sh
+++ scripts/ci/copy_to_release_bucket.sh
@@ -3,6 +3,8 @@ echo "=========================== Starting Copy to Release Bucket Script =======
 PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 
+BUILD_NUMBER=$1
+
 #
 # Copy from S3 Release bucket to S3 eu.dl bucket
 #
@@ -12,12 +14,12 @@ if [ -z "${RELEASE_VERSION}" ]; then
   exit 1
 fi
 
-SOURCE="s3://alfresco-artefacts-staging/share/${RELEASE_VERSION}"
-DESTINATION="s3://eu.dl.alfresco.com/release/share/${RELEASE_VERSION}"
+SOURCE="s3://alfresco-artefacts-staging/alfresco-content-services/release/${BRANCH_NAME}/${BUILD_NUMBER}"
+DESTINATION="s3://eu.dl.alfresco.com/release/enterprise/ACS/${RELEASE_VERSION:0:3}/${RELEASE_VERSION}/${BUILD_NUMBER}"
 
 printf "\n%s\n%s\n" "${SOURCE}" "${DESTINATION}"
 
-aws s3 cp --acl private --recursive "${SOURCE}" "${DESTINATION}"
+aws s3 cp --acl private --recursive --copy-props none "${SOURCE}" "${DESTINATION}"
 
 set +vex
 echo "=========================== Finishing Copy to Release Bucket Script =========================="
