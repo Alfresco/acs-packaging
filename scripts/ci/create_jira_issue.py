5130baef3c3890d5496097ce2c42526d6196071a
Damian Ujma <damian.ujma@hyland.com>
2023-04-13 09:37:47 +0200

    Fetch current release

--- scripts/ci/create_jira_issue.py
+++ scripts/ci/create_jira_issue.py
@@ -184,7 +184,7 @@ def set_current_release():
 
 
 if __name__ == "__main__":
-    CURRENT_RELEASE = "Oxford"
+    CURRENT_RELEASE = set_current_release()
 
     if not TEST_FAILURE:
         logging.info("Test passed, close issue if there is one")
