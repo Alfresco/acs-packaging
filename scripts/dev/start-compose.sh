f4f1f520c61d2ffd3518e0e5b6023e1c625dd142
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2023-05-25 12:01:38 +0200

    Revert "support new docker compose command"
    This reverts commit f6ac9a51aa98cc207e04652b7d1a2b35a76125e1.

--- scripts/dev/start-compose.sh
+++ scripts/dev/start-compose.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 set -x
 export TRANSFORMERS_TAG=$(mvn -f acs-packaging/pom.xml help:evaluate -Dexpression=dependency.alfresco-transform-core.version -q -DforceStdout)
 export TRANSFORM_ROUTER_TAG=$(mvn -f acs-packaging/pom.xml help:evaluate -Dexpression=dependency.alfresco-transform-service.version -q -DforceStdout)
 
 # .env files are picked up from project directory correctly on docker-compose 1.23.0+
-docker-compose -f acs-packaging/dev/docker-compose.yml up || docker compose -f acs-packaging/dev/docker-compose.yml up
+docker-compose -f acs-packaging/dev/docker-compose.yml up
