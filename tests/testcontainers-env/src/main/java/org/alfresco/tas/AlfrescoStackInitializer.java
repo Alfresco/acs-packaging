d3ba9df58067ff7f0207256cbe54e4951b806c1e
mikolajbrzezinski <mikolaj.brzezinski@hyland.com>
2023-08-04 15:12:29 +0200

    ACS-5734 Change transform-core memory

--- tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
+++ tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
@@ -41,7 +41,7 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
 
     public static final String CUSTOM_ALFRESCO_INDEX = "custom-alfresco-index";
     private final static Consumer<CreateContainerCmd> MEDIUM_RAM_LIMIT = cmd -> cmd.getHostConfig()
-            .withMemory((long) 2048*1024*1024)
+            .withMemory((long) 1536*1024*1024)
             .withMemorySwap((long) 3400*1024*1024);
 
     public static Network network;
@@ -349,6 +349,7 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
                        .withEnv("JAVA_OPTS", "-Xms1536m -Xmx2048m")
                        .withEnv("ACTIVEMQ_URL", "nio://activemq:61616")
                        .withEnv("FILE_STORE_URL", "http://shared-file-store:8099/alfresco/api/-default-/private/sfs/versions/1/file")
+                       .withCreateContainerCmdModifier(MEDIUM_RAM_LIMIT)
                        .withExposedPorts(8090)
                        .waitingFor(Wait.forListeningPort())
                        .withStartupTimeout(Duration.ofMinutes(2));
