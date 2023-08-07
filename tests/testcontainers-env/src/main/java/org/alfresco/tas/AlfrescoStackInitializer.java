569594d9b54dba396ebaf8cafb44c9978dafd2b7
mikolajbrzezinski <mikolaj.brzezinski@hyland.com>
2023-08-07 12:56:17 +0200

    ACS-5734 Change RAM limit

--- tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
+++ tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
@@ -44,6 +44,10 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
             .withMemory((long) 1536*1024*1024)
             .withMemorySwap((long) 3400*1024*1024);
 
+    private final static Consumer<CreateContainerCmd> LARGE_RAM_LIMIT = cmd -> cmd.getHostConfig()
+            .withMemory((long) 3600*1024*1024)
+            .withMemorySwap((long) 3400*1024*1024);
+
     public static Network network;
 
     public static GenericContainer alfresco;
@@ -103,7 +107,11 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
         startOrFail(transformCore, transformRouter);
 
         // We don't want Kibana to run on our CI, but it can be useful when investigating issues locally.
-
+        if (getSystemProperty("kibana", "false").equals("true"))
+        {
+            dashboardsContainer = createDashboardsContainer();
+            startOrFail(dashboardsContainer);
+        }
 
         liveIndexer = createLiveIndexingContainer();
 
@@ -321,7 +329,7 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
         return new OracleContainer(oracleImageName)
                 .withNetwork(network)
                 .withNetworkAliases("oracle")
-                .withCreateContainerCmdModifier(MEDIUM_RAM_LIMIT);
+                .withCreateContainerCmdModifier(LARGE_RAM_LIMIT);
     }
 
     private GenericContainer createSfsContainer()
@@ -345,7 +353,7 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
                        .withEnv("JAVA_OPTS", "-Xms1536m -Xmx2048m")
                        .withEnv("ACTIVEMQ_URL", "nio://activemq:61616")
                        .withEnv("FILE_STORE_URL", "http://shared-file-store:8099/alfresco/api/-default-/private/sfs/versions/1/file")
-                       .withCreateContainerCmdModifier(MEDIUM_RAM_LIMIT)
+                       .withCreateContainerCmdModifier(LARGE_RAM_LIMIT)
                        .withExposedPorts(8090)
                        .waitingFor(Wait.forListeningPort())
                        .withStartupTimeout(Duration.ofMinutes(2));
