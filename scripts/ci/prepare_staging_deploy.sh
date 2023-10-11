a2190e81c5a52b6f904b90e59635f6ca19d58419
Eva Vasques <eva.vasques@gmail.com>
2023-10-11 10:49:00 +0100

    Remove third party overrides functionality as its not implemented upstream in this version

--- scripts/ci/prepare_staging_deploy.sh
+++ scripts/ci/prepare_staging_deploy.sh
@@ -8,23 +8,10 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 # Identify latest annotated tag (latest version)
 export VERSION=$(git describe --abbrev=0 --tags)
 
-# Get third party license scripts.
-git clone --depth=1 https://github.com/Alfresco/third-party-license-overrides.git
-
 # Move the final artifacts to a single folder (deploy_dir) to be copied to S3
 mkdir -p deploy_dir
 cp distribution/target/alfresco.war deploy_dir
 cp distribution/target/*-distribution*.zip deploy_dir
-# Create third party license csv file and add it to the deploy directory.
-unzip deploy_dir/*-distribution*.zip -d deploy_dir/acs
-zippaths=""
-for file in `find deploy_dir/acs -name "*.amp" -o -name "*.war" -not -name "ROOT.war" -not -name "_vti_bin.war"`
-do
-    zippaths+="$file|"
-done
-zippaths=${zippaths::-1}
-python3 ./third-party-license-overrides/thirdPartyLicenseCSVCreator.py --zippaths "${zippaths}" --version "${VERSION}" --combined --output "deploy_dir"
-rm -rf deploy_dir/acs
 echo "Local deploy directory content:"
 ls -lA deploy_dir
 
@@ -32,29 +19,14 @@ ls -lA deploy_dir
 mkdir -p deploy_dir_share
 cp distribution-share/target/*.war deploy_dir_share
 cp distribution-share/target/*.zip deploy_dir_share
-# Generate third party license csv for Share.
-python3 ./third-party-license-overrides/thirdPartyLicenseCSVCreator.py --zip deploy_dir_share/*.war --version "${VERSION}" --combined --output "deploy_dir_share"
 echo "Local Share deploy directory content:"
 ls -lA deploy_dir_share
 
 # Create deploy directory for AGS.
 mkdir -p deploy_dir_ags
 cp distribution-ags/target/*.zip deploy_dir_ags
-# Generate third party license csv for AGS.
-unzip deploy_dir_ags/*.zip -d deploy_dir_ags/ags
-zippaths=""
-for file in `find deploy_dir_ags/ags -name "*.amp" -o -name "*.war"`
-do
-    zippaths+="$file|"
-done
-zippaths=${zippaths::-1}
-python3 ./third-party-license-overrides/thirdPartyLicenseCSVCreator.py --zippaths ${zippaths} --version "${VERSION}" --combined --output "deploy_dir_ags"
-rm -rf deploy_dir_ags/ags
 echo "Local AGS deploy directory content:"
 ls -lA deploy_dir_ags
-
-# Tidy up.
-rm -rf ./third-party-license-overrides
 
 popd
 set +vex
