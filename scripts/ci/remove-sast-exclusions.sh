7eabbacd1fb40570928db47253784ed834a890c8
Sara Aspery <sara.aspery@alfresco.com>
2024-12-19 09:03:48 +0000

    ACS-9044 Use temporary directory and allow any war file

--- scripts/ci/remove-sast-exclusions.sh
+++ scripts/ci/remove-sast-exclusions.sh
@@ -3,8 +3,8 @@ echo "=========================== Excluding Files from Veracode SAST ===========
 set -ex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
-# Copy alfresco.war file
-/bin/cp -f ./distribution/target/alfresco.war ./distribution/target/alfresco-reduced.war
+# Copy war file to temporary directory
+/bin/cp -f "$1" ./temp-dir-for-sast/reduced.war
 
 # Remove files to be excluded from Veracode SAST
 exclusions="./scripts/ci/SAST-exclusion-list.txt"
@@ -13,7 +13,7 @@ then
     while read -r line
     do
       echo "Removing WEB-INF/lib/$line"
-      zip -d ./packaging/war/target/alfresco-reduced.war "WEB-INF/lib/$line" || true
+      zip -d ./temp-dir-for-sast/reduced.war "WEB-INF/lib/$line" || true
     done < "$exclusions"
 else
     echo "No files to be excluded from SAST"
