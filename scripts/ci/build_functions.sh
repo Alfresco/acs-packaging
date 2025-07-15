7acfda4a43d4f9e520ba95f6fc95ed28f0a46bcd
bsayan2 <sayan.bhattacharya@hyland.com>
2025-07-15 12:34:37 +0530

    MNT-24893 pack testing

--- scripts/ci/build_functions.sh
+++ scripts/ci/build_functions.sh
@@ -225,7 +225,8 @@ function buildOtherDependentRepo() {
 
   pushd "$(dirname "${BASH_SOURCE[0]}")/../../../"
   cd "$(basename "${GIT_REPO%.git}")"
-  mvn -B -V -q clean install -DskipTests -Dmaven.javadoc.skip=true -Plocal
+#  mvn -B -V -q clean install -DskipTests -Dmaven.javadoc.skip=true -Plocal
+  mvn -B -ntp -V clean package -DskipTests -Dmaven.javadoc.skip=true "-Dimage.tag=latest"
   popd
 }
 
