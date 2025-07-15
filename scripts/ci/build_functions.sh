331bee1527f5dcce84f280a04a7d5cbf1eb1f864
bsayan2 <sayan.bhattacharya@hyland.com>
2025-07-15 14:11:29 +0530

    MNT-24893 pack testing

--- scripts/ci/build_functions.sh
+++ scripts/ci/build_functions.sh
@@ -102,12 +102,7 @@ function pullUpstreamTag() {
 function pullSameBranch() {
   local UPSTREAM_REPO="${1}"
 
-  if [[ "${UPSTREAM_REPO}" == *"github.com/Alfresco/alfresco-enterprise-repo.git" ]]; then
-    local SOURCE_BRANCH="fix/MNT-24893"
-  else
-    local SOURCE_BRANCH="$(identifyUpstreamSourceBranch "${UPSTREAM_REPO}")"
-  fi
-
+  local SOURCE_BRANCH="$(identifyUpstreamSourceBranch "${UPSTREAM_REPO}")"
 
   cloneRepo "${UPSTREAM_REPO}" "${SOURCE_BRANCH}"
 }
@@ -160,11 +155,7 @@ function pullAndBuildSameBranchOnUpstream() {
   local UPSTREAM_REPO="${1}"
   local EXTRA_BUILD_ARGUMENTS="${2}"
 
-  if [[ "${UPSTREAM_REPO}" == *"github.com/Alfresco/alfresco-enterprise-repo.git" ]]; then
-      local SOURCE_BRANCH="fix/MNT-24893"
-    else
-      local SOURCE_BRANCH="$(identifyUpstreamSourceBranch "${UPSTREAM_REPO}")"
-    fi
+  local SOURCE_BRANCH="fix/MNT-24893"
 
   cloneRepo "${UPSTREAM_REPO}" "${SOURCE_BRANCH}"
 
