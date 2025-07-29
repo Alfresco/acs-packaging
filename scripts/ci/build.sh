e2dcaaf9c1bddeeb3d4a6e6d25bc9a5f48547e33
Kacper Magdziarz <kacper.magdziarz@hyland.com>
2025-07-29 11:53:38 +0200

    [ACS-9736][pipeline_docker][skip tests] Release test docker img | Revert to even older changes

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -136,7 +136,7 @@ mvn dependency:copy -Dartifact=com.microsoft.sqlserver:mssql-jdbc:${MSSQL_JDBC_T
 ORACLE_JDBC_TAG=$(mvn help:evaluate -Dexpression=dependency.ojdbc.version -q -DforceStdout)
 mvn dependency:copy -Dartifact=com.oracle.database.jdbc:ojdbc11:${ORACLE_JDBC_TAG}:jar -DoutputDirectory=tests/environment/alfresco-with-jdbc-drivers
 
-docker build -t alfresco-repository-databases:latest -f tests/environment/alfresco-with-jdbc-drivers/alfresco.Dockerfile .
+#docker build -t alfresco-repository-databases:latest -f tests/environment/alfresco-with-jdbc-drivers/alfresco.Dockerfile .
 
 source tests/environment/.env
 
