8741a817820206b914f6be32a0919ae1c6a6cafd
SatyamSah5 <Satyam.Sah@hyland.com>
2025-09-16 10:36:40 +0530

    Add function to build Elasticsearch connector

--- scripts/ci/build_functions.sh
+++ scripts/ci/build_functions.sh
@@ -218,5 +218,16 @@ function copyArtifactToAnotherRepo() {
     -Dpackaging=${PACKAGING}
 }
 
+function buildElasticsearchConnector() {
+  local GIT_REPO="${1}"
+  local BRANCH="${2}"
+  cloneRepo "${GIT_REPO}" "${BRANCH}"
+
+  pushd "$(dirname "${BASH_SOURCE[0]}")/../../../"
+  cd "$(basename "${GIT_REPO%.git}")"
+  mvn -B -V -q clean install -DskipTests -Dmaven.javadoc.skip=true -Plocal -Pbuild-docker-images
+  popd
+}
+
 
 set -vx
