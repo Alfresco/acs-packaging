a2eced111576ec00cce3640652eb8cd5efb4a9ea
SatyamSah5 <Satyam.Sah@hyland.com>
2025-09-17 09:51:41 +0530

    Export JAVA_VERSION_TAG before building Docker images

--- scripts/ci/build_functions.sh
+++ scripts/ci/build_functions.sh
@@ -227,8 +227,8 @@ function buildElasticsearchConnector() {
   cd "$(basename "${GIT_REPO%.git}")"
   mvn -B -V -q clean install -DskipTests -Dmaven.javadoc.skip=true
   chmod +x ./scripts/ci/buildDockerImagesWithJavaTagCi.sh
-  JAVA_VERSION_TAG="21"
-  ./scripts/ci/buildDockerImagesWithJavaTagCi.sh "$JAVA_VERSION_TAG"
+  export JAVA_VERSION_TAG="21"
+  ./scripts/ci/buildDockerImagesWithJavaTagCi.sh
   popd
 }
 
