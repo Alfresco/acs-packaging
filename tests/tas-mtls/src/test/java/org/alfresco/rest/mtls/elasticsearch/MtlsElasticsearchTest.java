3243067e3a73ac5c6ecc49b2647202d7655a6df7
cezary-witkowski <cezary.witkowski@hyland.com>
2026-04-24 12:47:05 +0200

    [ACS-7965] PMD fix

--- tests/tas-mtls/src/test/java/org/alfresco/rest/mtls/elasticsearch/MtlsElasticsearchTest.java
+++ tests/tas-mtls/src/test/java/org/alfresco/rest/mtls/elasticsearch/MtlsElasticsearchTest.java
@@ -4,7 +4,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 import org.alfresco.rest.MtlsRestTest;
 
-@SuppressWarnings("ClassWithoutTestCases")
+@SuppressWarnings("PMD.TestClassWithoutTestCases")
 @ContextConfiguration("classpath:alfresco-mtls-elasticsearch-context.xml")
 public class MtlsElasticsearchTest extends MtlsRestTest
 {}
