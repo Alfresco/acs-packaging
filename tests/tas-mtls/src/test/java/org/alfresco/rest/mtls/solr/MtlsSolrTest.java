57de896505227d88d8668c789bbec0f2beae36aa
Cezary Witkowski <cezary.witkowski@hyland.com>
2026-04-27 07:55:12 +0200

    [ACS-7965] Support for mTLS connection with Search Enterprise backport 25.N (#3404)
    * [ACS-7965] Implemented elasticsearch mtls tests
    
    * [ACS-7965] Add secrets to baseline
    
    * [ACS-7965] Fixed issues with connector->es mtls
    
    * [ACS-7965] Precommit secrets
    
    * [ACS-7965] Do not commit local builds
    
    * [ACS-7965] Refactoring
    
    * [ACS-7965] Remove duplications
    
    * [ACS-7965] Fix mistake and npe in CI
    
    * [ACS-7965] Fix missing file, use custom live indexing that supports mtls in es client
    
    * [ACS-7965] Test improvements
    
    * [ACS-7965] Investigate CI failures
    
    * [ACS-7965] Fixed custom checkout
    
    * [ACS-7965] Tests on CI should pass now
    
    * [ACS-7965] Review fixes
    
    * [ACS-7965] Review fixes
    
    * [ACS-7965] pre commit
    
    * [ACS-7965] Review fixes
    
    * [ACS-7965] Refactoring
    
    * [ACS-7965] Fix secrets
    
    * [ACS-7965] Added opensearch mtls tests
    
    * [ACS-7965] Reformat
    
    * [ACS-7965] Turn off sniffer, improve comment
    
    * [ACS-7965] Secrets fix
    
    * [ACS-7965] Simplify live-indexing config in mtls tests
    
    * [ACS-7965] Pre commit
    
    * [ACS-7965] Prepare for merge
    
    * [ACS-7965] Change setup to match documentation and what client has
    
    * [ACS-7965] Update secrets
    
    * [ACS-7965] PMD and reduce failures in live-indexing
    
    * [ACS-7965] Remove hostname verification disabling tests for elasticsearch/opensearch
    
    * [ACS-7965] PMD fix
    # Conflicts:
    #	.secrets.baseline
    #	tests/environment/docker-compose-mtls.yml

---
+++ tests/tas-mtls/src/test/java/org/alfresco/rest/mtls/solr/MtlsSolrTest.java
@@ -0,0 +1,10 @@
+package org.alfresco.rest.mtls.solr;
+
+import org.springframework.test.context.ContextConfiguration;
+
+import org.alfresco.rest.MtlsRestTest;
+
+@SuppressWarnings("PMD.TestClassWithoutTestCases")
+@ContextConfiguration("classpath:alfresco-mtls-solr-context.xml")
+public class MtlsSolrTest extends MtlsRestTest
+{}
