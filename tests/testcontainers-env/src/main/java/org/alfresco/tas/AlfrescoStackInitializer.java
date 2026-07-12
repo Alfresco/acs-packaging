b9de488efcd57936b29bc682e55f1972a0405b28
bsayan2 <sayan.bhattacharya@hyland.com>
2026-07-12 05:49:54 +0000

    Fix test

--- tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
+++ tests/testcontainers-env/src/main/java/org/alfresco/tas/AlfrescoStackInitializer.java
@@ -190,6 +190,7 @@ public class AlfrescoStackInitializer implements ApplicationContextInitializer<C
                         "SPRING_ACTIVEMQ_PASSWORD", "admin",
                         "JAVA_TOOL_OPTIONS", "-Xmx1g",
                         "ALFRESCO_ACCEPTEDCONTENTMEDIATYPESCACHE_BASEURL", "http://transform-core-aio:8090/transform/config"));
+        env.put("ALFRESCO_REINDEX_PROMETHEUS_SCRAPE_INTERVAL_SECONDS", "1");
         return env;
     }
 
