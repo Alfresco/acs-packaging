1cdbab2060872a21e4115d6fb41f820556dd47b2
Damian Ujma <damian.ujma@hyland.com>
2026-08-18 15:54:35 +0200

    Revert "ACS-12380 Revert "ACS-12380 Migrate to GitHub App Tokens (#3551)"  (#3562)"
    This reverts commit e81c495a5fd40e4e25210bd0e54dace8957cafc5.

--- scripts/ci/build_functions.sh
+++ scripts/ci/build_functions.sh
@@ -20,7 +20,10 @@ function cloneRepo() {
 
   rm -rf "$(basename "${REPO%.git}")"
 
-  git clone -b "${TAG_OR_BRANCH}" --depth=1 "https://${GIT_USERNAME}:${GIT_PASSWORD}@${REPO}"
+  { set +x; } 2>/dev/null
+  local AUTH="x-access-token:${APP_TOKEN}@"
+  git clone -b "${TAG_OR_BRANCH}" --depth=1 "https://${AUTH}${REPO}"
+  { set -x; } 2>/dev/null
 
   popd >/dev/null
 }
@@ -76,7 +79,10 @@ function remoteBranchExists() {
   local REMOTE_REPO="${1}"
   local BRANCH="${2}"
 
-  git ls-remote --exit-code --heads "https://${GIT_USERNAME}:${GIT_PASSWORD}@${REMOTE_REPO}" "${BRANCH_NAME}" &>/dev/null
+  { set +x; } 2>/dev/null
+  local AUTH="x-access-token:${APP_TOKEN}@"
+  git ls-remote --exit-code --heads "https://${AUTH}${REMOTE_REPO}" "${BRANCH_NAME}" &>/dev/null
+  { set -x; } 2>/dev/null
 }
 
 function identifyUpstreamSourceBranch() {
@@ -175,7 +181,9 @@ function retieveLatestTag() {
 
   local LOCAL_PATH="/tmp/$(basename "${REPO%.git}")"
 
-  git clone -q -b "${BRANCH_NAME}" "https://${GIT_USERNAME}:${GIT_PASSWORD}@${REPO}" "${LOCAL_PATH}"
+  { set +x; } 2>/dev/null
+  git clone -q -b "${BRANCH_NAME}" "https://x-access-token:${APP_TOKEN}@${REPO}" "${LOCAL_PATH}"
+  { set -x; } 2>/dev/null
 
   pushd "${LOCAL_PATH}" >/dev/null
   git describe --abbrev=0 --tags
